48ddf2741d42a734bbeb8648643a330cd13284f9
burianek <burianek@marbes.cz>
2015-08-04 14:24:33 +0200

    Project license,scm

--- elza-core/src/main/java/cz/tacr/elza/domain/FaChange.java
+++ elza-core/src/main/java/cz/tacr/elza/domain/FaChange.java
@@ -8,8 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import cz.tacr.elza.ax.IdObject;
+import cz.req.ax.IdObject;
 
 /**
  * @author by Ondřej Buriánek, burianek@marbes.cz.
