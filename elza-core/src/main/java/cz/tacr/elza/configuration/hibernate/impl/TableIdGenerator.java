6e85255d40ea0a6d92aec1adb20104a782e27958
pubal <jaroslav.pubal@marbes.cz>
2015-09-08 10:03:50 +0200

    ELZA, generovani vice id

--- elza-core/src/main/java/cz/tacr/elza/configuration/hibernate/impl/TableIdGenerator.java
+++ elza-core/src/main/java/cz/tacr/elza/configuration/hibernate/impl/TableIdGenerator.java
@@ -1,11 +1,18 @@
 package cz.tacr.elza.configuration.hibernate.impl;
 
-import java.util.Properties;
-
 import org.hibernate.MappingException;
 import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.spi.SessionImplementor;
 import org.hibernate.id.enhanced.TableGenerator;
 import org.hibernate.type.Type;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Properties;
 
 
 /**
@@ -26,5 +33,35 @@ public class TableIdGenerator extends TableGenerator {
         params.setProperty(INCREMENT_PARAM, "1");
         //params.setProperty(OPT_PARAM, "pooled");
         super.configure(type, params, dialect);
+    }
+
+    @Override
+    public Serializable generate(SessionImplementor session, Object obj) {
+        Serializable id = getReplicatedId(obj);
+        if (id == null) id = super.generate(session, obj);
+        return id;
+    }
+
+    private Serializable getReplicatedId(Object obj) {
+        try {
+            Class<?> cls = obj.getClass();
+
+            ArrayList<Field> fields = new ArrayList<>();
+            fields.addAll(Arrays.asList(cls.getDeclaredFields()));
+            if (cls.getSuperclass() != null)
+                fields.addAll(Arrays.asList(cls.getSuperclass().getDeclaredFields()));
+
+            for (Field field : fields) {
+                if (field.getAnnotation(Id.class) != null && field.getAnnotation(GeneratedValue.class) != null) {
+                    field.setAccessible(true);
+                    Serializable id = (Serializable) field.get(obj);
+                    if (id != null) return id;
+                }
+            }
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
