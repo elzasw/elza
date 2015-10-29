3ce48285905a87b14c9371ffe4266a7e8665b0ce
Martin Šlapa <slapa@marbes.cz>
2015-10-29 09:04:24 +0100

    ELZA-207 - Zahrnout v testech nový attribut

--- elza-core/src/test/java/cz/tacr/elza/controller/AbstractRestTest.java
+++ elza-core/src/test/java/cz/tacr/elza/controller/AbstractRestTest.java
@@ -203,6 +203,7 @@ public abstract class AbstractRestTest {
     protected static final Integer DATA_TYPE_COORDINATES = 7;
     protected static final Integer DATA_TYPE_PARTY_REF = 8;
     protected static final Integer DATA_TYPE_RECORD_REF = 9;
+    protected static final Integer DATA_TYPE_DECIMAL = 10;
     // END ARRANGEMENT MANAGER CONSTANTS
 
     // RULE MANAGER CONSTANTS
