91281e1f13b3815ddaa35cec60fa24aa879ed1f8
Martin Šlapa <slapa@marbes.cz>
2015-11-26 16:51:04 +0100

    ELZA-239 - Zapojení vyhodnocování pravidel do metody getDescriptionItemTypesForNode + příprava na pravidla pro další metody

--- elza-core/src/main/java/cz/tacr/elza/drools/RulesExecutor.java
+++ elza-core/src/main/java/cz/tacr/elza/drools/RulesExecutor.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.slf4j.Logger;ó
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
