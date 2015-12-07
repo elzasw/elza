b258634ec453a41210e675b613fa17fb7cfa9ec1
Martin Šlapa <slapa@marbes.cz>
2015-12-07 12:33:33 +0100

    ELZA-266 - ELZA: úprava datace

--- elza-api/src/main/java/cz/tacr/elza/api/ArrDataUnitdate.java
+++ elza-api/src/main/java/cz/tacr/elza/api/ArrDataUnitdate.java
@@ -5,33 +5,93 @@ import java.io.Serializable;
 
 /**
  * hodnota atributu archivního popisu typu strojově zpracovatelná datace.
+ *
  * @author Martin Šlapa
  * @since 1.9.2015
  */
 public interface ArrDataUnitdate<CT extends ArrCalendarType> extends Serializable {
 
+    /**
+     * @return vrací datum "od" v iso formátu
+     */
     String getValueFrom();
 
+
+    /**
+     * @param valueFrom datum "od" v iso formátu
+     */
     void setValueFrom(String valueFrom);
 
+
+    /**
+     * @return je datum "od" přibližný?
+     */
     Boolean getValueFromEstimated();
 
+
+    /**
+     * @param valueFromEstimated je datum "od" přibližný?
+     */
     void setValueFromEstimated(Boolean valueFromEstimated);
 
+
+    /**
+     * @return vrací datum "do" v iso formátu
+     */
     String getValueTo();
 
+
+    /**
+     * @param valueTo datum "do" v iso formátu
+     */
     void setValueTo(String valueTo);
 
+
+    /**
+     * @return je datum "do přibližný?
+     */
     Boolean getValueToEstimated();
 
+
+    /**
+     * @param valueToEstimated je datum "do přibližný?
+     */
     void setValueToEstimated(Boolean valueToEstimated);
 
+
+    /**
+     * @return identifikátor typu kalendáře
+     */
     Integer getCalendarTypeId();
 
+
+    /**
+     * @param calendarTypeId identifikátor typu kalendáře
+     */
     void setCalendarTypeId(Integer calendarTypeId);
 
+
+    /**
+     * Možnosti hodnot formátu:
+     * - století: C
+     * - rok: Y
+     * - rok/měsíc: YM
+     * - datum: D
+     * - datum a čas: DT
+     *
+     * Formát může být zadán:
+     * - jako jedna hodnota (např. Y)
+     * - jako interval (např. Y-Y)
+     * - jako polointernval (např. Y-)
+     *
+     * @return formát, jakým způsobem se má zobrazovat výstup
+     */
     String getFormat();
 
+
+    /**
+     * @param format formát, jakým způsobem se má zobrazovat výstup
+     */
     void setFormat(String format);
 
 }
