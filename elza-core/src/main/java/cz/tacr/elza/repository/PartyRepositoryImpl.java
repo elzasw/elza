518eecbd576f39c682151930dd47a12b676d70f2
Martin Kužel <martin.kuzel@marbes.cz>
2015-12-16 12:33:12 +0100

    ELZA-298 - RegistryManager - zodolnit vstupy find*.

--- elza-core/src/main/java/cz/tacr/elza/repository/PartyRepositoryImpl.java
+++ elza-core/src/main/java/cz/tacr/elza/repository/PartyRepositoryImpl.java
@@ -1,25 +1,22 @@
 package cz.tacr.elza.repository;
 
 import cz.tacr.elza.domain.ParParty;
-import cz.tacr.elza.domain.ParPartyName;
 import cz.tacr.elza.domain.ParPartyType;
 import cz.tacr.elza.domain.RegRecord;
 import cz.tacr.elza.domain.RegVariantRecord;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.CriteriaUpdate;
-import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
-
 import java.util.List;
 
 /**
@@ -35,7 +32,7 @@ public class PartyRepositoryImpl implements PartyRepositoryCustom {
 
     @Override
     public List<ParParty> findPartyByTextAndType(final String searchRecord, final Integer registerTypeId,
-                                         final Integer firstResult, final Integer maxResults, Boolean originator) {
+                                         final Integer firstResult, final Integer maxResults, final Boolean originator) {
 
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<ParParty> query = builder.createQuery(ParParty.class);
@@ -43,8 +40,11 @@ public class PartyRepositoryImpl implements PartyRepositoryCustom {
 
         Predicate condition = preparefindRegRecordByTextAndType(searchRecord, registerTypeId, record, builder, originator);
 
-        Order order = builder.asc(record.get(ParParty.ABSTRACT_PARTY_ID));
-        query.select(record).where(condition).orderBy(order).distinct(true);
+        query.select(record).distinct(true);
+        if (condition != null) {
+            Order order = builder.asc(record.get(ParParty.ABSTRACT_PARTY_ID));
+            query.where(condition).orderBy(order);
+        }
 
         return entityManager.createQuery(query)
                 .setFirstResult(firstResult)
@@ -53,7 +53,7 @@ public class PartyRepositoryImpl implements PartyRepositoryCustom {
     }
 
     @Override
-    public long findPartyByTextAndTypeCount(final String searchRecord, final Integer registerTypeId, Boolean originator) {
+    public long findPartyByTextAndTypeCount(final String searchRecord, final Integer registerTypeId, final Boolean originator) {
 
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Long> query = builder.createQuery(Long.class);
@@ -61,10 +61,13 @@ public class PartyRepositoryImpl implements PartyRepositoryCustom {
 
         Predicate condition = preparefindRegRecordByTextAndType(searchRecord, registerTypeId, record, builder, originator);
 
-        query.select(builder.countDistinct(record)).where(condition);
+        query.select(builder.countDistinct(record));
+        if (condition != null) {
+            query.where(condition);
+        }
 
         return entityManager.createQuery(query)
-                .getSingleResult();
+            .getSingleResult();
     }
 
     /**
@@ -74,10 +77,10 @@ public class PartyRepositoryImpl implements PartyRepositoryCustom {
      * @param partyTypeId       typ záznamu
      * @param builder           buider pro vytváření podmínek
      * @param originator        původce - true, není původce - false, null - neaplikuje filtr - obě možnosti
-     * @return                  výsledné podmínky pro dotaz
+     * @return                  výsledné podmínky pro dotaz, nebo null pokud není za co filtrovat
      */
     private Predicate preparefindRegRecordByTextAndType(final String searchRecord, final Integer partyTypeId,
-                        final Root<ParParty> party, final CriteriaBuilder builder, Boolean originator) {
+                        final Root<ParParty> party, final CriteriaBuilder builder, final Boolean originator) {
 
         final String searchString = (searchRecord != null ? searchRecord.toLowerCase() : null);
 
@@ -89,7 +92,7 @@ public class PartyRepositoryImpl implements PartyRepositoryCustom {
         String searchValue = "%" + searchString + "%";
 
         Predicate condition = null;
-        if (searchString != null) {
+        if (StringUtils.isNotBlank(searchString)) {
             condition = builder.or(
                     builder.like(builder.lower(record.get(RegRecord.RECORD)), searchValue),
                     builder.like(builder.lower(record.get(RegRecord.CHARACTERISTICS)), searchValue),
