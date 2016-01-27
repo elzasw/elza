342f950c2a28550359178252e69a33da0699a2f9
Pavel Stánek <pavel.stanek@marbes.cz>
2016-01-27 18:01:56 +0100

    ELZA-495 - Ukládat a načítat aktuální stav aplikace - zmena fa na store

--- elza-web/src/main/resources/frontend/stores/app/arr/arrRegion.jsx
+++ elza-web/src/main/resources/frontend/stores/app/arr/arrRegion.jsx
@@ -2,6 +2,7 @@ import * as types from 'actions/constants/actionTypes';
 import {indexById, selectedAfterClose} from 'stores/app/utils.jsx'
 
 import nodes from './nodes'
+import {fa, faInitState} from './fa'
 import faTree from './faTree'
 import nodeSetting from './nodeSetting'
 
@@ -16,21 +17,11 @@ const initialState = {
 function selectFaTab(state, action) {
     var index = indexById(state.fas, action.fa.id);
     if (index == null) {    // není zatím v seznamu, přidáme jí tam
-        var faItem = Object.assign({}, action.fa, {
-            faTree: {...faTree(action.fa.faTree, action)},
-            faTreeMovementsLeft: {...faTree(action.fa.faTreeMovementsLeft, action)},
-            faTreeMovementsRight: {...faTree(action.fa.faTreeMovementsRight, action)},
-            nodes: nodes(undefined, action)
-        });
-
-        faItem.faTreeMovementsLeft.multipleSelection = true;
-        faItem.faTreeMovementsLeft.multipleSelectionOneLevel = true;
-
         return {
             ...state,
             fas: [
                 ...state.fas,
-                faItem
+                faInitState(action.fa)
             ],
             activeIndex: state.fas.length
         }
@@ -39,20 +30,6 @@ function selectFaTab(state, action) {
             ...state,
             activeIndex: index
         }
-    }
-}
-
-function updateFaTree(fa, action) {
-    switch (action.area) {
-        case types.FA_TREE_AREA_MAIN:
-            fa.faTree = faTree(fa.faTree, action)
-            break;
-        case types.FA_TREE_AREA_MOVEMENTS_LEFT:
-            fa.faTreeMovementsLeft = faTree(fa.faTreeMovementsLeft, action)
-            break;
-        case types.FA_TREE_AREA_MOVEMENTS_RIGHT:
-            fa.faTreeMovementsRight = faTree(fa.faTreeMovementsRight, action)
-            break;
     }
 }
 
@@ -68,83 +45,7 @@ export default function arrRegion(state = initialState, action) {
             return {...state, extendedView: action.enable}
         case types.FA_FA_TREE_REQUEST:
         case types.FA_FA_TREE_RECEIVE:
-            var index = indexById(state.fas, action.versionId, "versionId");
-            if (index != null) {
-                var newFa = Object.assign({}, state.fas[index]);
-                updateFaTree(newFa, action);
-
-                return {
-                    ...state,
-                    fas: [
-                        ...state.fas.slice(0, index),
-                        newFa,
-                        ...state.fas.slice(index + 1)
-                    ]
-                }
-            } else {
-                return state;
-            }
         case types.FA_FA_TREE_FULLTEXT_RESULT:
-            var index = indexById(state.fas, action.versionId, "versionId");
-            if (index != null) {
-                var newFa = Object.assign({}, state.fas[index]);
-                updateFaTree(newFa, action);
-
-                return {
-                    ...state,
-                    fas: [
-                        ...state.fas.slice(0, index),
-                        newFa,
-                        ...state.fas.slice(index + 1)
-                    ]
-                }
-            } else {
-                return result;
-            }
-        case types.FA_FA_TREE_FULLTEXT_CHANGE:
-        case types.FA_FA_TREE_FOCUS_NODE:
-        case types.FA_FA_TREE_EXPAND_NODE:
-        case types.FA_FA_TREE_COLLAPSE_NODE:
-        case types.FA_FA_TREE_SELECT_NODE:
-        case types.GLOBAL_CONTEXT_MENU_HIDE:
-            var newFa = Object.assign({}, state.fas[state.activeIndex]);
-            updateFaTree(newFa, action);
-
-            return {
-                ...state,
-                fas: [
-                    ...state.fas.slice(0, state.activeIndex),
-                    newFa,
-                    ...state.fas.slice(state.activeIndex + 1)
-                ]
-            }
-        case types.FA_FA_SELECT_SUBNODE:
-            var newFa = Object.assign({}, state.fas[state.activeIndex]);
-            updateFaTree(newFa, action);
-            newFa.nodes = nodes(state.fas[state.activeIndex].nodes, action);
-
-            return {
-                ...state,
-                fas: [
-                    ...state.fas.slice(0, state.activeIndex),
-                    newFa,
-                    ...state.fas.slice(state.activeIndex + 1)
-                ]
-            }
-        case types.FA_FA_SUBNODES_NEXT:
-        case types.FA_FA_SUBNODES_PREV:
-        case types.FA_FA_SUBNODES_NEXT_PAGE:
-        case types.FA_FA_SUBNODES_PREV_PAGE:
-        case types.FA_FA_CLOSE_NODE_TAB:
-        case types.FA_FA_SELECT_NODE_TAB:
-            return {
-                ...state,
-                fas: [
-                    ...state.fas.slice(0, state.activeIndex),
-                    Object.assign({}, state.fas[state.activeIndex], {nodes: nodes(state.fas[state.activeIndex].nodes, action)}),
-                    ...state.fas.slice(state.activeIndex + 1)
-                ]
-            }
         case types.FA_NODE_INFO_REQUEST:
         case types.FA_NODE_INFO_RECEIVE:
         case types.FA_SUB_NODE_FORM_REQUEST:
@@ -161,32 +62,41 @@ export default function arrRegion(state = initialState, action) {
         case types.FA_SUB_NODE_FORM_VALUE_RESPONSE:
         case types.FA_SUB_NODE_INFO_REQUEST:
         case types.FA_SUB_NODE_INFO_RECEIVE:
-            var faIndex = indexById(state.fas, action.versionId, "versionId");
-            if (faIndex != null) {
+        case types.FA_NODE_CHANGE:
+            var index = indexById(state.fas, action.versionId, "versionId");
+            if (index != null) {
                 return {
                     ...state,
                     fas: [
-                        ...state.fas.slice(0, faIndex),
-                        Object.assign({}, state.fas[faIndex], {nodes: nodes(state.fas[faIndex].nodes, action)}),
-                        ...state.fas.slice(faIndex + 1)
+                        ...state.fas.slice(0, index),
+                        fa(state.fas[index], action),
+                        ...state.fas.slice(index + 1)
                     ]
                 }
             } else {
                 return state;
             }
-        case types.FA_NODE_CHANGE:
-            var faIndex = indexById(state.fas, action.versionId, "versionId");
-            if (faIndex != null) {
-                return {
-                    ...state,
-                    fas: [
-                        ...state.fas.slice(0, faIndex),
-                        Object.assign({}, state.fas[faIndex], {nodes: nodes(state.fas[faIndex].nodes, action)}),
-                        ...state.fas.slice(faIndex + 1)
-                    ]
-                }
-            } else {
-                return state;
+        case types.FA_FA_TREE_FULLTEXT_CHANGE:
+        case types.FA_FA_TREE_FOCUS_NODE:
+        case types.FA_FA_TREE_EXPAND_NODE:
+        case types.FA_FA_TREE_COLLAPSE_NODE:
+        case types.FA_FA_TREE_SELECT_NODE:
+        case types.GLOBAL_CONTEXT_MENU_HIDE:
+        case types.FA_FA_SELECT_SUBNODE:
+        case types.FA_FA_SUBNODES_NEXT:
+        case types.FA_FA_SUBNODES_PREV:
+        case types.FA_FA_SUBNODES_NEXT_PAGE:
+        case types.FA_FA_SUBNODES_PREV_PAGE:
+        case types.FA_FA_CLOSE_NODE_TAB:
+        case types.FA_FA_SELECT_NODE_TAB:
+            var index = state.activeIndex;
+            return {
+                ...state,
+                fas: [
+                    ...state.fas.slice(0, index),
+                    fa(state.fas[index], action),
+                    ...state.fas.slice(index + 1)
+                ]
             }
         case types.FA_CLOSE_FA_TAB:
             var index = indexById(state.fas, action.fa.id);
@@ -257,7 +167,6 @@ export default function arrRegion(state = initialState, action) {
             }
 
         case types.CHANGE_CONFORMITY_INFO:
-
             var index = indexById(state.fas, action.findingAidVersionId);
 
             // změna se ho netýká, vracím původní stav
@@ -265,23 +174,14 @@ export default function arrRegion(state = initialState, action) {
                 return state;
             }
 
-            var faTreeChange = faTree(state.fas[index].faTree, action);
-            var nodesChange = nodes(state.fas[index].nodes, action);
-
-            // nezměnil se stav podřízených, nemusím nic měnit
-            if (faTreeChange === state.fas[index].faTree && nodesChange === state.fas[index].nodes) {
-                return state;
-            }
-
             return {
                 ...state,
                 fas: [
                     ...state.fas.slice(0, index),
-                    Object.assign({}, state.fas[index], {faTree: faTreeChange, nodes: nodesChange}),
+                    fa(state.fas[index], action),
                     ...state.fas.slice(index + 1)
                 ]
-            }
-
+            }            
         default:
             return state
     }
---
+++ elza-web/src/main/resources/frontend/stores/app/arr/fa.jsx
@@ -0,0 +1,90 @@
+import * as types from 'actions/constants/actionTypes';
+import {indexById} from 'stores/app/utils.jsx'
+import faTree from './faTree'
+import nodes from './nodes'
+
+export function faInitState(faWithVersion) {
+    var result = {
+        ...faWithVersion,
+        isFetching: false,
+        fetched: false,
+        dirty: false,
+        faTree: faTree(undefined, {type: ''}),
+        faTreeMovementsLeft: faTree(undefined, {type: ''}),
+        faTreeMovementsRight: faTree(undefined, {type: ''}),
+        nodes: nodes(undefined, {type: ''})
+    }
+
+    result.faTreeMovementsLeft = {...result.faTreeMovementsLeft};
+    result.faTreeMovementsLeft.multipleSelection = true;
+    result.faTreeMovementsLeft.multipleSelectionOneLevel = true;
+
+    return result;
+}
+
+function updateFaTree(state, action) {
+    switch (action.area) {
+        case types.FA_TREE_AREA_MAIN:
+            state.faTree = faTree(state.faTree, action)
+            break;
+        case types.FA_TREE_AREA_MOVEMENTS_LEFT:
+            state.faTreeMovementsLeft = faTree(state.faTreeMovementsLeft, action)
+            break;
+        case types.FA_TREE_AREA_MOVEMENTS_RIGHT:
+            state.faTreeMovementsRight = faTree(state.faTreeMovementsRight, action)
+            break;
+    }
+}
+
+const faInitialState = {
+}
+
+export function fa(state = nodeInitialState, action) {
+    switch (action.type) {
+        case types.FA_FA_TREE_REQUEST:
+        case types.FA_FA_TREE_RECEIVE:
+        case types.FA_FA_TREE_FULLTEXT_RESULT:
+        case types.FA_FA_TREE_FULLTEXT_CHANGE:
+        case types.FA_FA_TREE_FOCUS_NODE:
+        case types.FA_FA_TREE_EXPAND_NODE:
+        case types.FA_FA_TREE_COLLAPSE_NODE:
+        case types.FA_FA_TREE_SELECT_NODE:
+        case types.GLOBAL_CONTEXT_MENU_HIDE:
+            var result = {...state};
+            updateFaTree(result, action);
+            return result;
+        case types.FA_FA_SELECT_SUBNODE:
+            var result = {...state, nodes: nodes(state.nodes, action)}
+            updateFaTree(result, action);
+            return result;
+        case types.FA_FA_SUBNODES_NEXT:
+        case types.FA_FA_SUBNODES_PREV:
+        case types.FA_FA_SUBNODES_NEXT_PAGE:
+        case types.FA_FA_SUBNODES_PREV_PAGE:
+        case types.FA_FA_CLOSE_NODE_TAB:
+        case types.FA_FA_SELECT_NODE_TAB:
+        case types.FA_NODE_CHANGE:
+            return {...state, nodes: nodes(state.nodes, action)}
+        case types.FA_NODE_INFO_REQUEST:
+        case types.FA_NODE_INFO_RECEIVE:
+        case types.FA_SUB_NODE_FORM_REQUEST:
+        case types.FA_SUB_NODE_FORM_RECEIVE:
+        case types.FA_SUB_NODE_FORM_VALUE_CHANGE:
+        case types.FA_SUB_NODE_FORM_VALUE_CHANGE_SPEC:
+        case types.FA_SUB_NODE_FORM_VALUE_VALIDATE_RESULT:
+        case types.FA_SUB_NODE_FORM_VALUE_BLUR:
+        case types.FA_SUB_NODE_FORM_VALUE_FOCUS:
+        case types.FA_SUB_NODE_FORM_VALUE_ADD:
+        case types.FA_SUB_NODE_FORM_VALUE_DELETE:
+        case types.FA_SUB_NODE_FORM_DESC_ITEM_TYPE_DELETE:
+        case types.FA_SUB_NODE_FORM_DESC_ITEM_TYPE_ADD:
+        case types.FA_SUB_NODE_FORM_VALUE_RESPONSE:
+        case types.FA_SUB_NODE_INFO_REQUEST:
+        case types.FA_SUB_NODE_INFO_RECEIVE:
+            return {...state, nodes: nodes(state.nodes, action)}
+        case types.CHANGE_CONFORMITY_INFO:
+            return {...state, faTree: faTree(state.faTree, action), nodes: nodes(state.nodes, action)}
+        default:
+            return state;
+    }
+}
