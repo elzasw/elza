37a36364a7d57642eec1b5e90c4e0edecce1fbda
Tomáš Kubový <tomas.kubovy@marbes.cz>
2016-02-05 10:57:10 +0100

    updateRecord vrací i variatní hesla

--- elza-core/src/main/java/cz/tacr/elza/controller/RegistryController.java
+++ elza-core/src/main/java/cz/tacr/elza/controller/RegistryController.java
@@ -329,8 +329,11 @@ public class RegistryController {
         RegRecord newRecordDO = registryService.saveRecord(recordDO, false);
 
         ParParty recordParty = partyService.findParPartyByRecord(newRecordDO);
-        return factoryVo.createRegRecord(newRecordDO, recordParty == null ? null : recordParty.getPartyId(), false,
-                null);
+        RegRecordVO result = factoryVo
+                .createRegRecord(newRecordDO, recordParty == null ? null : recordParty.getPartyId(), false,
+                        null);
+        result.setVariantRecords(factoryVo.createRegVariantRecords(variantRecordRepository.findByRegRecordId(record.getRecordId())));
+        return result;
     }
 
 
