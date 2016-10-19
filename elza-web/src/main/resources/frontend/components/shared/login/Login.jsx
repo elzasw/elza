6c8ddeb458b1e91c1f19974c44081f500390cad6
Petr Compel <petr.compel@marbes.cz>
2016-10-19 11:42:26 +0200

    Úprava login dialogu - funkční enter - update syntaxe

--- elza-web/src/main/resources/frontend/components/shared/login/Login.jsx
+++ elza-web/src/main/resources/frontend/components/shared/login/Login.jsx
@@ -16,11 +16,15 @@ const defaultLogin = {
 };
 
 class Login extends AbstractReactComponent {
-    state = {
+    defaultState = {
         username: '',
         password: '',
         ...defaultLogin, // TODO: Smazat až bude potřeba
         error: null
+    };
+
+    state = {
+        ...this.defaultState
     };
 
     handleChange = (field, event) => {
@@ -40,7 +44,7 @@ class Login extends AbstractReactComponent {
             } catch (ex) {
                 console.error("Error calling login callback.", ex)
             }
-            this.setState({username: defaultLogin.username, password: defaultLogin.password, error: null});
+            this.replaceState(this.defaultState);
         }).catch((err) => {
             this.setState({error: err.data.message});
         });
@@ -50,22 +54,20 @@ class Login extends AbstractReactComponent {
         const {login} = this.props;
         const {error, username, password} = this.state;
 
-        return (
-            <div className="login-container">
-                {!login.logged && <ModalDialogWrapper className="login" ref='wrapper' title={i18n('login.form.title')}>
-                    <Form onSubmit={this.handleLogin}>
-                        <Modal.Body>
-                            {error && <div className="error">{error}</div>}
-                            <FormInput type="text" value={username} onChange={this.handleChange.bind(this, 'username')} label={i18n('login.field.username')} />
-                            <FormInput type="password" value={password} onChange={this.handleChange.bind(this, 'password')} label={i18n('login.field.password')} />
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button type="submit" onClick={this.handleLogin}>{i18n('login.action.login')}</Button>
-                        </Modal.Footer>
-                    </Form>
-                </ModalDialogWrapper>}
-            </div>
-        );
+        return <div className="login-container">
+            {!login.logged && <ModalDialogWrapper className="login" ref='wrapper' title={i18n('login.form.title')}>
+                <Form onSubmit={this.handleLogin}>
+                    <Modal.Body>
+                        {error && <div className="error">{error}</div>}
+                        <FormInput type="text" value={username} onChange={this.handleChange.bind(this, 'username')} label={i18n('login.field.username')} />
+                        <FormInput type="password" value={password} onChange={this.handleChange.bind(this, 'password')} label={i18n('login.field.password')} />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button type="submit" onClick={this.handleLogin}>{i18n('login.action.login')}</Button>
+                    </Modal.Footer>
+                </Form>
+            </ModalDialogWrapper>}
+        </div>;
     }
 }
 
