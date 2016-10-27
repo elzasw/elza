1d9dc7757629d7e5619549ed6b8ff568e80bc6df
Martin Šlapa <martin.slapa@marbes.cz>
2016-10-27 08:33:31 +0200

    ELZA-1207 - Hromadné akce - doplnění COPY

--- elza-core/src/main/java/cz/tacr/elza/domain/ArrItemFormattedText.java
+++ elza-core/src/main/java/cz/tacr/elza/domain/ArrItemFormattedText.java
@@ -2,6 +2,8 @@ package cz.tacr.elza.domain;
 
 import cz.tacr.elza.domain.interfaces.IArrItemStringValue;
 
+import java.util.Objects;
+
 /**
  * Rozšiřuje atribut archivního popisu o jeho hodnotu.
  *
@@ -25,5 +27,19 @@ public class ArrItemFormattedText extends ArrItemData implements cz.tacr.elza.ap
     @Override
     public String toString() {
         return value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ArrItemFormattedText that = (ArrItemFormattedText) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), value);
     }
 }
--- elza-core/src/main/java/cz/tacr/elza/domain/ArrItemRecordRef.java
+++ elza-core/src/main/java/cz/tacr/elza/domain/ArrItemRecordRef.java
@@ -1,5 +1,7 @@
 package cz.tacr.elza.domain;
 
+import java.util.Objects;
+
 /**
  * Rozšiřuje atribut archivního popisu o jeho hodnotu.
  *
@@ -23,5 +25,19 @@ public class ArrItemRecordRef extends ArrItemData implements cz.tacr.elza.api.Ar
     @Override
     public String toString() {
         return (record != null ) ? record.getRecord() : null;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ArrItemRecordRef that = (ArrItemRecordRef) o;
+        return Objects.equals(record, that.record);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), record);
     }
 }
