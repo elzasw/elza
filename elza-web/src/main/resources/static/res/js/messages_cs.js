701d263f6ed379e4cd5a44cdf478f0679c88fbc3
Martin Šlapa <martin.slapa@marbes.cz>
2016-12-01 15:44:51 +0100

    ELZA-1165 - OutputGeneratorService.java - oprava překlepu

--- elza-web/src/main/resources/static/res/js/messages_cs.js
+++ elza-web/src/main/resources/static/res/js/messages_cs.js
@@ -37,7 +37,7 @@ var messages = {
 
     "search.input.search": "Vyhledat...",
     "search.not.found": "nenalezeno",
-    "search.found": "(0})",
+    "search.found": "({0})",
     "search.found.more": "({0}/{1})",
 
     "global.validation.required": "Pole je povinné",
