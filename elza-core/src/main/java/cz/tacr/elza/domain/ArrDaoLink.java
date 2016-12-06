94598bc6b501ad553ba8180692aab84b59af46ff
Martin Šlapa <martin.slapa@marbes.cz>
2016-12-06 16:55:41 +0100

    ELZA-1292 - Vytvořit struktury pro digitalizáty

---
+++ elza-core/src/main/java/cz/tacr/elza/domain/ArrDaoLink.java
@@ -0,0 +1,91 @@
+package cz.tacr.elza.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+/**
+ * Implementace {@link cz.tacr.elza.api.ArrDaoLink}
+ *
+ * @author Martin Šlapa
+ * @since 06.12.2016
+ */
+@Table
+@Entity(name = "arr_dao_link")
+public class ArrDaoLink implements cz.tacr.elza.api.ArrDaoLink<ArrDao, ArrChange, ArrNode> {
+
+    @Id
+    @GeneratedValue
+    private Integer daoLinkId;
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = ArrNode.class)
+    @JoinColumn(name = "nodeId", nullable = false)
+    private ArrNode node;
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = ArrDao.class)
+    @JoinColumn(name = "daoId", nullable = false)
+    private ArrDao dao;
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = ArrChange.class)
+    @JoinColumn(name = "createChangeId", nullable = false)
+    private ArrChange createChange;
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = ArrChange.class)
+    @JoinColumn(name = "deleteChangeId")
+    private ArrChange deleteChange;
+
+    @Override
+    public Integer getDaoLinkId() {
+        return daoLinkId;
+    }
+
+    @Override
+    public void setDaoLinkId(final Integer daoLinkId) {
+        this.daoLinkId = daoLinkId;
+    }
+
+    @Override
+    public ArrNode getNode() {
+        return node;
+    }
+
+    @Override
+    public void setNode(final ArrNode node) {
+        this.node = node;
+    }
+
+    @Override
+    public ArrDao getDao() {
+        return dao;
+    }
+
+    @Override
+    public void setDao(final ArrDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public ArrChange getCreateChange() {
+        return createChange;
+    }
+
+    @Override
+    public void setCreateChange(final ArrChange createChange) {
+        this.createChange = createChange;
+    }
+
+    @Override
+    public ArrChange getDeleteChange() {
+        return deleteChange;
+    }
+
+    @Override
+    public void setDeleteChange(final ArrChange deleteChange) {
+        this.deleteChange = deleteChange;
+    }
+}
