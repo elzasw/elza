950b69db71d376eac3b33d6a02aa40e4f6ec3c07
Martin Šlapa <martin.slapa@marbes.cz>
2017-02-02 16:07:09 +0100

    ELZA-1415 - 24.1. LC-JR: Dropdown Provést akci v záložce obaly v pravém panelu zůstává v IE stále otevřený

--- elza-web/src/main/resources/frontend/components/shared/fixed-dropdown-button/FixedDropDownButton.jsx
+++ elza-web/src/main/resources/frontend/components/shared/fixed-dropdown-button/FixedDropDownButton.jsx
@@ -13,7 +13,6 @@ require ('./FixedDropDownButton.less');
 var FixedDropDownButton = class FixedDropDownButton extends AbstractReactComponent {
     constructor(props) {
         super(props);
-        this.bindMethods("setSizes")
         this.state = {marginSide: 0, marginTop: 0};
     }
 
@@ -29,7 +28,7 @@ var FixedDropDownButton = class FixedDropDownButton extends AbstractReactCompone
 
         dropMenu[1].style.display = "block";
         const calHeight = dropMenu[1].offsetHeight;
-        dropMenu[1].style.display = null;
+        dropMenu[1].style.display = "";
 
         const newState = {
             marginSide: this.props.pullRight ?
