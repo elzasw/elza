72fa79650fa1dafa5ce03f11c87dfaef4af40676
Martin Šlapa <martin.slapa@marbes.cz>
2017-02-08 11:30:27 +0100

    ELZA-1421 - Problémy s dialogem osob/rejstříků (problém se zavíráním dialogu křížkem)

--- elza-web/src/main/resources/frontend/components/shared/dialog/ModalDialogWrapper.jsx
+++ elza-web/src/main/resources/frontend/components/shared/dialog/ModalDialogWrapper.jsx
@@ -8,6 +8,12 @@ import {setInputFocus} from 'components/Utils.jsx'
  * Obal modálního dialogu
  */
 export default class ModalDialogWrapper extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.hide = false;
+    }
+
     componentDidMount() {
         this.setState({}, () => {
             if (this.refs.modalBody) {
@@ -23,14 +29,25 @@ export default class ModalDialogWrapper extends React.Component {
         this.refs.modal._onHide();
     };
 
+    /**
+     * Zajistí aby se callback na zavření dialogu zavolal vždy jen jednou! Bootstrap bug.
+     */
+    onHide = (e) => {
+        const {onHide} = this.props;
+        if (!this.hide) {
+            onHide(e);
+            this.hide = true;
+        }
+    };
+
     render() {
         const {title, onHide, className, children} = this.props;
 
         const renderHeader = title !== null;
 
         return (
-            <Modal backdrop='static' className={className} ref='modal' show={true} onHide={onHide}>
-                {renderHeader && <Modal.Header closeButton onHide={onHide}>
+            <Modal backdrop='static' className={className} ref='modal' show={true} onHide={this.onHide}>
+                {renderHeader && <Modal.Header closeButton onHide={this.onHide}>
                     <Modal.Title>{title}</Modal.Title>
                 </Modal.Header>}
 
