74111e26ef54981b311b52821f1caf926764feec
Karel Šoupa <karel.soupa@marbes.cz>
2017-10-24 12:14:44 +0200

    ELZA-1556 - zruseni select page

--- elza-web/src/main/resources/frontend/components/form/UsageForm.jsx
+++ elza-web/src/main/resources/frontend/components/form/UsageForm.jsx
@@ -1,37 +1,41 @@
 import React from 'react';
-import {Button, Col, Form, Modal, Row} from 'react-bootstrap';
-import {WebApi} from '../../actions/WebApi';
-import {connect} from 'react-redux';
-import {usageFundTreeReceive} from '../../actions/arr/globalFundTree';
+import { Button, Col, Form, Modal, Row } from 'react-bootstrap';
+import { WebApi } from '../../actions/WebApi';
+import { connect } from 'react-redux';
+import { usageFundTreeReceive } from '../../actions/arr/globalFundTree';
 import FundTreeUsage from '../arr/FundTreeUsage';
 import './UsageForm.less';
 import RegistryField from '../registry/RegistryField';
 import * as types from 'actions/constants/ActionTypes.js';
 import ToggleContent from '../shared/toggle-content/ToggleContent';
-import {AREA_PARTY_LIST, partyDetailClear, partyDetailFetchIfNeeded, partyListFilter} from '../../actions/party/party';
+import {
+    AREA_PARTY_LIST,
+    partyDetailClear,
+    partyDetailFetchIfNeeded,
+    partyListFilter
+} from '../../actions/party/party';
 import {
     AREA_REGISTRY_LIST,
     registryDetailClear,
     registryDetailFetchIfNeeded,
     registryListFilter
 } from '../../actions/registry/registry';
-import {modalDialogHide, modalDialogShow} from '../../actions/global/modalDialog';
-import RegistrySelectPage from 'pages/select/RegistrySelectPage.jsx'
+import { modalDialogHide, modalDialogShow } from '../../actions/global/modalDialog';
+import RegistrySelectPage from 'pages/select/RegistrySelectPage.jsx';
 import classNames from 'classnames';
-import {MODAL_DIALOG_VARIANT} from 'constants.jsx';
+import { MODAL_DIALOG_VARIANT } from 'constants.jsx';
 import storeFromArea from '../../shared/utils/storeFromArea';
-import objectById from '../../shared/utils/objectById';
 import i18n from '../i18n';
-import PartyField from "../party/PartyField";
-import PartySelectPage from "../../pages/select/PartySelectPage";
-import * as perms from "../../actions/user/Permission";
-import {decorateAutocompleteValue} from "../arr/nodeForm/DescItemUtils";
-import {createFundRoot, getFundFromFundAndVersion, getParentNode} from "../arr/ArrUtils";
-import {fundSelectSubNode} from "../../actions/arr/node";
-import {withRouter} from 'react-router';
-import {selectFundTab} from "../../actions/arr/fund";
-import {deletePackage} from "../../actions/admin/packages";
-import {fundsSelectFund} from "../../actions/fund/fund";
+import PartyField from '../party/PartyField';
+import PartySelectPage from '../../pages/select/PartySelectPage';
+import * as perms from '../../actions/user/Permission';
+import { createFundRoot, getParentNode } from '../arr/ArrUtils';
+import { fundSelectSubNode } from '../../actions/arr/node';
+import { withRouter } from 'react-router';
+import { selectFundTab } from '../../actions/arr/fund';
+import { fundsSelectFund } from '../../actions/fund/fund';
+import { fundTreeFetch } from '../../actions/arr/fundTree';
+import { FUND_TREE_AREA_MAIN } from '../../actions/constants/ActionTypes';
 
 class RegistryUsageForm extends React.Component {
     static propTypes = {
@@ -42,34 +46,34 @@ class RegistryUsageForm extends React.Component {
     rootPartyIdOffset = 0.2;
     nodePartyIdOffset = 0.3;
     manyItemsIdOffset = 0.4;
-    manyItemsLabel = i18n("registry.usage.tooMany");
+    manyItemsLabel = i18n('registry.usage.tooMany');
     expandFundThreshold = 500;
 
     state = {
         selectedReplacementNode: null,
         usageCount: 0,
-        data: {},
+        data: {}
     };
 
     componentDidMount() {
-        const {detail, data} = this.props;
+        const { detail, data } = this.props;
         if (detail.id) {
-                if (data) {
-                    this.setState({
-                        usageCount: this.countOccurences(data),
-                        data
-                    });
+            if (data) {
+                this.setState({
+                    usageCount: this.countOccurences(data),
+                    data
+                });
 
-                    this.props.dispatch(
-                        usageFundTreeReceive(
-                            [
-                                ...this.formatDataForTree(data.funds, 'fund'),
-                                ...this.formatDataForTree(data.parties, 'party')
-                            ],
-                            this.getDefaultExpandedIds(data)
-                        )
-                    );
-                }
+                this.props.dispatch(
+                    usageFundTreeReceive(
+                        [
+                            ...this.formatDataForTree(data.funds, 'fund'),
+                            ...this.formatDataForTree(data.parties, 'party')
+                        ],
+                        this.getDefaultExpandedIds(data)
+                    )
+                );
+            }
         }
     }
 
@@ -92,7 +96,7 @@ class RegistryUsageForm extends React.Component {
     countOccurencesInAS = as => {
         return as.reduce((sum, jp) => {
             return sum + this.countOccurencesForNode(jp);
-        },0)
+        }, 0);
     };
 
     countOccurencesForNode = node => node.occurrences && node.occurrences.length;
@@ -108,7 +112,11 @@ class RegistryUsageForm extends React.Component {
                 name: item.name,
                 depth: 1,
                 hasChildren: type === 'fund',
-                count: type === 'fund' ? item.nodes && this.countOccurencesInAS(item.nodes) : this.countOccurencesForNode(item)
+                count:
+                    type === 'fund'
+                        ? item.nodes && this.countOccurencesInAS(item.nodes)
+                        : this.countOccurencesForNode(item),
+                link: type === 'party'
             });
             if (item.nodes) {
                 if (item.nodes.length < this.expandFundThreshold) {
@@ -129,8 +137,9 @@ class RegistryUsageForm extends React.Component {
                     processedFunds.push({
                         id: item.id + this.manyItemsIdOffset,
                         depth: 2,
-                        name: this.manyItemsLabel
-                    })
+                        name: this.manyItemsLabel,
+                        icon: 'fa-fw',
+                    });
                 }
             }
         });
@@ -139,7 +148,7 @@ class RegistryUsageForm extends React.Component {
     }
 
     expandNode(node) {
-        const {fundTreeUsage} = this.props;
+        const { fundTreeUsage } = this.props;
 
         const nodes = [
             ...this.formatDataForTree(this.state.data.funds, 'fund'),
@@ -192,124 +201,72 @@ class RegistryUsageForm extends React.Component {
     };
 
     handleChoose = selectedReplacementNode => {
-        this.setState({selectedReplacementNode});
-    };
-
-    handleSelectModuleRegistry = ({onSelect, filterText, value}) => {
-        const {
-            hasSpecification,
-            descItem,
-            registryList,
-            partyList,
-        } = this.props;
-        const open = (hasParty = false) => {
-            if (hasParty) {
-                this.props.dispatch(
-                    partyListFilter({
-                        ...partyList.filter,
-                        text: filterText,
-                        itemSpecId: hasSpecification ? descItem.descItemSpecId : null
-                    })
-                );
-                this.props.dispatch(partyDetailClear());
-            }
-            this.props.dispatch(
-                registryListFilter({
-                    ...registryList.filter,
-                    text: filterText,
-                    itemSpecId: hasSpecification ? descItem.descItemSpecId : null
-                })
-            );
-            this.props.dispatch(registryDetailFetchIfNeeded(value ? value.id : null));
-            this.props.dispatch(
-                modalDialogShow(
-                    this,
-                    null,
-                    <RegistrySelectPage
-                        hasParty={hasParty}
-                        onSelect={data => {
-                            onSelect(data);
-                            if (hasParty) {
-                                this.props.dispatch(partyListFilter({text: null, type: null, itemSpecId: null}));
-                                this.props.dispatch(partyDetailClear());
-                            }
-                            this.props.dispatch(
-                                registryListFilter({
-                                    text: null,
-                                    registryParentId: null,
-                                    registryTypeId: null,
-                                    versionId: null,
-                                    itemSpecId: null,
-                                    parents: [],
-                                    typesToRoot: null
-                                })
-                            );
-                            this.props.dispatch(registryDetailClear());
-                        }}
-                    />,
-                    classNames(MODAL_DIALOG_VARIANT.FULLSCREEN, MODAL_DIALOG_VARIANT.NO_HEADER)
-                )
-            );
-        };
-
-        if (hasSpecification) {
-            WebApi.specificationHasParty(descItem.descItemSpecId).then(open);
-        } else {
-            open();
-        }
-    };
-
-    handleSelectModuleParty = ({onSelect, filterText, value}) => {
-        const {partyList:{filter}} = this.props;
-        this.props.dispatch(partyListFilter({...filter, text:filterText}));
-        this.props.dispatch(partyDetailFetchIfNeeded(value ? value.id : null));
-        this.props.dispatch(modalDialogShow(this, null, <PartySelectPage
-            onSelect={(data) => {
-                onSelect(data);
-                this.props.dispatch(partyListFilter({text:null, type:null, itemSpecId: null}));
-                this.props.dispatch(partyDetailClear());
-            }}
-        />, classNames(MODAL_DIALOG_VARIANT.FULLSCREEN, MODAL_DIALOG_VARIANT.NO_HEADER)));
+        this.setState({ selectedReplacementNode });
     };
 
     canReplace() {
-        const {userDetail} = this.props;
-        const {selectedReplacementNode} = this.state;
+        const { userDetail, detail } = this.props;
+        const { selectedReplacementNode } = this.state;
 
-        if (selectedReplacementNode) {
+        if (selectedReplacementNode && detail.id !== selectedReplacementNode.id) {
             return userDetail.hasOne(perms.REG_SCOPE_WR_ALL, {
-                    type: perms.REG_SCOPE_WR,
-                    scopeId: selectedReplacementNode.scopeId
-                });
-            }
+                type: perms.REG_SCOPE_WR,
+                scopeId: selectedReplacementNode.scopeId
+            });
+        }
         return false;
     }
 
-    handleLinkClick = (node) => {
+    handleLinkClick = node => {
         console.log(node);
         this.props.dispatch(modalDialogHide());
-        if (node.type === "fund") {
-            this.props.history.push("/arr");
+        if (node.type === 'fund') {
+            this.props.history.push('/arr');
             this.handleShowInArr(node);
-            //this.callFundSelectSubNode(node)
+        } else if(node.type === 'party') {
+            this.props.history.push('/party');
+            this.props.dispatch(partyDetailFetchIfNeeded(node.propertyId));
         }
     };
 
     handleShowInArr(node) {
-        const {data} = this.state;
-        const fundId = data.funds.find((n) => n.id === node.origParent).id;
+        const { data } = this.state;
+        const fundId = data.funds.find(n => n.id === node.origParent).id;
 
-        WebApi.getFundDetail(fundId).then((fund) => {
+        WebApi.getFundDetail(fundId).then(fund => {
             this.props.dispatch(fundsSelectFund(fund.id));
             this.props.dispatch(selectFundTab(fund));
-            //TODO Otevírání As
-            //this.props.dispatch(fundSelectSubNode(node.versionId, node.propertyId, {}, false, null, true));
+            this.props.dispatch(this.callFundSelectSubNode(node, false, true, fund));
         });
     }
 
+    getActiveIndex(arrRegion) {
+        return arrRegion.activeIndex !== null ? arrRegion.funds[arrRegion.activeIndex] : null;
+    }
+
+    callFundSelectSubNode(node, openNewTab, ensureItemVisible, fund) {
+        return (dispatch, getState) => {
+            const { arrRegion } = getState();
+            const activeFund = this.getActiveIndex(arrRegion);
+            dispatch(fundTreeFetch(FUND_TREE_AREA_MAIN, fund.versionId, node.propertyId, activeFund.expandedIds)).then(() => {
+                const { arrRegion } = getState();
+                const activeFund = this.getActiveIndex(arrRegion);
+
+                const nodeFromTree = activeFund.fundTree.nodes.find(n => n.id === node.propertyId);
+
+                let parentNode = getParentNode(nodeFromTree, activeFund.fundTree.nodes);
+
+                if (parentNode === null) {
+                    parentNode = createFundRoot(fund);
+                }
+                dispatch(fundSelectSubNode(fund.versionId, node.id, parentNode, openNewTab, null, ensureItemVisible));
+            });
+        };
+    }
+
     render() {
-        const {detail, fundTreeUsage, onReplace} = this.props;
-        const {selectedReplacementNode} = this.state;
+        const { detail, fundTreeUsage, onReplace } = this.props;
+        const { selectedReplacementNode } = this.state;
         return (
             <Modal.Body className="reg-usage-form">
                 <h4>
@@ -319,66 +276,62 @@ class RegistryUsageForm extends React.Component {
                     {i18n('registry.registryUsageCount')} {this.state.usageCount}
                 </label>
                 {fundTreeUsage &&
-                <FundTreeUsage
-                    handleOpenCloseNode={this.handleOpenCloseNode}
-                    className="fund-tree-container-fixed"
-                    cutLongLabels={true}
-                    ref="treeUsage"
-                    showCountStats={true}
-                    onLinkClick={this.handleLinkClick}
-                    {...fundTreeUsage}
-                />}
-                {this.state.usageCount > 0 && <ToggleContent withText text={this.props.replaceText}>
-                    <Row>
-                        <Col xs={10}>
-                            {this.props.type === "registry" ? <RegistryField
-                                value={this.state.selectedReplacementNode}
-                                onChange={this.handleChoose}
-                                onBlur={() => {
-                                }}
-                                detail
-                                onSelectModule={this.handleSelectModuleRegistry}
-                            /> :
-                            <PartyField
-                                value={this.state.selectedReplacementNode}
-                                detail
-                                onBlur={() => {
-                                }}
-                                onSelectModule={this.handleSelectModuleParty}
-                                onChange={this.handleChoose}
-
-                            />}
-                        </Col>
-                        <Col xs={2}>
-                            <Button
-                                onClick={() => onReplace(selectedReplacementNode)}
-                                disabled={!this.canReplace()}
-                            >
-                                {i18n('registry.replace')}
-                            </Button>
-                        </Col>
-                    </Row>
-                </ToggleContent>}
+                    <FundTreeUsage
+                        handleOpenCloseNode={this.handleOpenCloseNode}
+                        className="fund-tree-container-fixed"
+                        cutLongLabels={true}
+                        ref="treeUsage"
+                        showCountStats={true}
+                        onLinkClick={this.handleLinkClick}
+                        {...fundTreeUsage}
+                    />}
+                {this.state.usageCount > 0 &&
+                    <ToggleContent withText text={this.props.replaceText}>
+                        <Row>
+                            <Col xs={10}>
+                                {this.props.type === 'registry'
+                                    ? <RegistryField
+                                          value={this.state.selectedReplacementNode}
+                                          onChange={this.handleChoose}
+                                          onBlur={() => {}}
+                                      />
+                                    : <PartyField
+                                          value={this.state.selectedReplacementNode}
+                                          onChange={this.handleChoose}
+                                          onBlur={() => {}}
+                                      />}
+                            </Col>
+                            <Col xs={2}>
+                                <Button
+                                    onClick={() => onReplace(selectedReplacementNode)}
+                                    disabled={!this.canReplace()}
+                                >
+                                    {i18n('registry.replace')}
+                                </Button>
+                            </Col>
+                        </Row>
+                    </ToggleContent>}
             </Modal.Body>
         );
     }
 
     componentWillUnmount() {
         this.props.dispatch({
-            type: types.FUND_FUND_TREE_INVALIDATE,
+            type: types.FUND_FUND_TREE_INVALIDATE
         });
-
     }
 }
 
-export default withRouter(connect((state, props) => {
-    const registryList = storeFromArea(state, AREA_REGISTRY_LIST);
-    const partyList = storeFromArea(state, AREA_PARTY_LIST);
+export default withRouter(
+    connect((state, props) => {
+        const registryList = storeFromArea(state, AREA_REGISTRY_LIST);
+        const partyList = storeFromArea(state, AREA_PARTY_LIST);
 
-    return {
-        fundTreeUsage: state.arrRegion.globalFundTree.fundTreeUsage,
-        registryList,
-        partyList,
-        userDetail: state.userDetail
-    };
-})(RegistryUsageForm));
+        return {
+            fundTreeUsage: state.arrRegion.globalFundTree.fundTreeUsage,
+            registryList,
+            partyList,
+            userDetail: state.userDetail
+        };
+    })(RegistryUsageForm)
+);
