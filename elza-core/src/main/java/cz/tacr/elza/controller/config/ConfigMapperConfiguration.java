fbf74f121ca381c1e5e1165db48a5784f84f8cb2
Martin Šlapa <martin.slapa@marbes.cz>
2018-02-21 08:50:46 +0100

    ELZA-1598 - ELZA MT05 - Přejmenování datových objektů - oprava

--- elza-core/src/main/java/cz/tacr/elza/controller/config/ConfigMapperConfiguration.java
+++ elza-core/src/main/java/cz/tacr/elza/controller/config/ConfigMapperConfiguration.java
@@ -778,6 +778,7 @@ public class ConfigMapperConfiguration {
         mapperFactory.classMap(RulItemTypeExt.class, RulDescItemTypeExtVO.class).byDefault()
                 .field("itemTypeId", "id")
                 .field("rulItemSpecList", "descItemSpecs")
+                .field("structuredTypeId", "structureTypeId")
                 .register();
         mapperFactory.classMap(RulItemTypeExt.class, ItemTypeLiteVO.class).byDefault()
                 .field("itemTypeId", "id")
@@ -1089,7 +1090,9 @@ public class ConfigMapperConfiguration {
                         super.mapBtoA(itemStructureVO, itemStructureRef, context);
                         itemStructureRef.setStructuredObject(itemStructureVO.getValue() == null ? null : structureDataRepository.findOne(itemStructureVO.getValue()));
                     }
-                }).byDefault().register();
+                })
+                .field("structuredObject", "structureData")
+                .byDefault().register();
         mapperFactory.classMap(ArrDataFileRef.class, ArrItemFileRefVO.class).customize(
                 new CustomMapper<ArrDataFileRef, ArrItemFileRefVO>() {
                     @Override
