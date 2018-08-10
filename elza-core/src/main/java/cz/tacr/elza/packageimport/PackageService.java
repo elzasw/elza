c70c780d0ebe6535df54dbb996f162d609cd57f2
slapa <slapa@marbes.cz>
2018-08-10 10:14:53 +0200

    ELZA-1632 - ELZA MT10 Strukturovaný popis přístupového bodu a jmen přístupového bodu - dokončení přesunu - oprava pravidel
    # Conflicts:
    #	elza-core/src/main/java/cz/tacr/elza/repository/ApFragmentRuleRepository.java
    #	elza-core/src/main/java/cz/tacr/elza/repository/ApRuleRepository.java
    #	elza-core/src/test/java/cz/tacr/elza/controller/ApControllerTest.java
    #	rules-simple-dev/src/ap_fragment_type.xml
    #	rules-simple-dev/src/ap_rule_system.xml
    #	rules-simple-dev/src/rules/APBase-body.drl
    #	rules-simple-dev/src/rules/APBase-name.drl
    #	rules-simple-dev/src/rules/StatZastupce.drl
    #	rules-simple-dev/src/scripts/APBase.groovy
    #	rules-simple-dev/src/scripts/StatZastupce.groovy

--- elza-core/src/main/java/cz/tacr/elza/packageimport/PackageService.java
+++ elza-core/src/main/java/cz/tacr/elza/packageimport/PackageService.java
@@ -2531,6 +2531,10 @@ public class PackageService {
         partyNameFormTypeRepository.deleteByRulPackage(rulPackage);
         relationRoleTypeRepository.deleteByRulPackage(rulPackage);
         settingsRepository.deleteByRulPackage(rulPackage);
+        fragmentRuleRepository.deleteByRulPackage(rulPackage);
+        fragmentTypeRepository.deleteByRulPackage(rulPackage);
+        ruleRepository.deleteByRulPackage(rulPackage);
+        ruleSystemRepository.deleteByRulPackage(rulPackage);
         packageRepository.delete(rulPackage);
 
         entityManager.flush();
