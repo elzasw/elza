ac82ccbee149d966eef9823aac73bf39180c3325
Petr Pytelka <petr.pytelka@lightcomp.cz>
2018-09-25 08:59:10 +0200

    Bug 4603 - Nefunkční export x import pro běžného uživatele

--- elza-core/src/main/java/cz/tacr/elza/controller/DEExportController.java
+++ elza-core/src/main/java/cz/tacr/elza/controller/DEExportController.java
@@ -1,24 +1,19 @@
 package cz.tacr.elza.controller;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.google.common.net.HttpHeaders;
 
 import cz.tacr.elza.dataexchange.output.DEExportParams;
 import cz.tacr.elza.dataexchange.output.DEExportParams.FundSections;
@@ -70,22 +65,7 @@ public class DEExportController {
             throw new UnsupportedOperationException("Export transformation not implemented");
         }
 
-        // file headers
-        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        response.setContentType(MediaType.APPLICATION_XML_VALUE);
-        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=elza-data.xml");
-
-        // cache headers
-        response.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate");
-        response.setHeader(HttpHeaders.PRAGMA, "no-cache");
-        response.setDateHeader(HttpHeaders.EXPIRES, 0);
-
-        // write response
-        try (ServletOutputStream os = response.getOutputStream()) {
-            response.flushBuffer();
-            exportService.exportXmlData(os, params);
-            response.flushBuffer();
-        }
+        exportService.exportXmlData(response, params);
     }
 
     public static class DEExportParamsVO extends DEExportParams {
