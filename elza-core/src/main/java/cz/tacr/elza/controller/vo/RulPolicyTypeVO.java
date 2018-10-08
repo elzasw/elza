b0b844566c1cbd5195774ffa3d52b548a35f5977
Petr Pytelka <petr.pytelka@lightcomp.cz>
2018-10-08 17:36:42 +0200

    Odstraneni duplicit

--- elza-core/src/main/java/cz/tacr/elza/controller/vo/RulPolicyTypeVO.java
+++ elza-core/src/main/java/cz/tacr/elza/controller/vo/RulPolicyTypeVO.java
@@ -3,42 +3,11 @@ package cz.tacr.elza.controller.vo;
 /**
  * VO pro třídu RulPolicyType.
  *
- * @author Martin Šlapa
- * @since 29.03.2016
  */
-public class RulPolicyTypeVO {
-
-    private Integer id;
-
-    private String code;
-
-    private String name;
+public class RulPolicyTypeVO
+        extends BaseCodeVo {
 
     private Integer ruleSetId;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(final Integer id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(final String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
 
     public Integer getRuleSetId() {
         return ruleSetId;
