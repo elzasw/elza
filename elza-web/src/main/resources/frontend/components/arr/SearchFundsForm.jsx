5b66e45efc424767258ab666d15ed611fe77cc7f
David Slabý <xslaby@gmail.com>
2018-11-13 15:37:31 +0100

    MCV-51128: Search Modal - render AS nodes list

--- elza-web/src/main/resources/frontend/components/arr/SearchFundsForm.jsx
+++ elza-web/src/main/resources/frontend/components/arr/SearchFundsForm.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {AbstractReactComponent, FormInput, i18n} from 'components/shared';
+import {AbstractReactComponent, FormInput, Icon, i18n} from 'components/shared';
 import {Modal} from 'react-bootstrap';
 import {decorateFormField, submitForm} from 'components/form/FormUtils.jsx'
 import Search from "../shared/search/Search";
 import {connect} from "react-redux";
+import './SearchFundsForm.less';
 
 //  Actions
 import * as types from '../../actions/constants/ActionTypes.js';
@@ -37,7 +38,109 @@ class SearchFundsForm extends AbstractReactComponent {
         this.dispatch(fundModalFulltextChange(''));
     };
 
+    /**
+     * Renderování uzlu.
+     * @param node {Object} uzel
+     * @return {Object} view
+     */
+    renderNode = (node) => {
+        const {onNodeDoubleClick, onOpenCloseNode, onContextMenu} = this.props;
+
+        const expanded = this.props.expandedIds[node.id];
+
+        const clickProps = {
+            onClick: (e)=>this.handleNodeClick(node, false, e),
+            onDoubleClick: (e)=>this.handleNodeDoubleClick(node, false, e),
+        };
+
+        const expColCls = 'exp-col ' + (expanded ? 'fa fa-minus-square-o' : 'fa fa-plus-square-o');
+        expCol = <span className={expColCls} onClick={onOpenCloseNode.bind(this, node, !expanded)}></span>
+
+        let active = false;
+        active |= this.props.selectedId === node.id;
+        if (this.props.selectedIds && this.props.selectedIds[node.id]) {
+            active = true
+        }
+        const cls = classNames({
+            node: true,
+            opened: expanded,
+            closed: !expanded,
+            active: active,
+            focus: this.props.focusId === node.id,
+            "node-color": this.props.colorCoded
+        });
+        const iconClass = classNames({
+            "node-icon": true,
+            "node-icon-color": this.props.colorCoded
+        });
+        var levels = createReferenceMark(node, clickProps);
+
+        let name = node.name ? node.name : i18n('fundTree.node.name.undefined', node.id);
+        const title = name;
+        if (this.props.cutLongLabels) {
+            if (name.length > TREE_NAME_MAX_CHARS) {
+                name = name.substring(0, TREE_NAME_MAX_CHARS - 3) + '...'
+            }
+        }
+        let style = {};
+        let backgroundColor, color;
+
+        if(this.props.colorCoded){
+            if(colorMap[node.icon]){
+                backgroundColor = colorMap[node.icon].background;
+                color = colorMap[node.icon].color;
+            } else {
+                backgroundColor = colorMap["default"].background;
+                color = colorMap["default"].color;
+            }
+            style = {
+                backgroundColor:backgroundColor,
+                color:color
+            };
+        }
+        let icon = getGlyph(node.icon);
+        let iconRemap = {
+            "fa-folder-o":"folder",
+            "ez-serie":"serie",
+            "fa-sitemap":"sitemap",
+            "fa-file-text-o":"fileText",
+            "ez-item-part-o":"fileTextPart",
+            "fa-exclamation-triangle":"triangleExclamation"
+        };
+        if(iconRemap[icon] && this.props.colorCoded){
+            icon = iconRemap[icon];
+        }
+
+        return <div key={node.id} className={cls}>
+            {levels}
+            {expCol}
+            <Icon {...clickProps} className={iconClass} style={style} fill={backgroundColor} stroke="none" glyph={icon}/>
+            <div
+                title={title}
+                className='node-label'
+                {...clickProps}
+                onContextMenu={onContextMenu ? onContextMenu.bind(this, node) : null}>
+                {name}
+                {this.props.onLinkClick && node.link && <Icon glyph="fa-sign-out fa-lg" onClick={() => this.props.onLinkClick(node)}/>}
+            </div>
+        </div>;
+    };
+
     render() {
+        let { data } = this.props;
+
+        // mockup data
+        data = [];
+        const node = {
+            depth: 1,
+            hasChildren: true,
+            icon: "fa-sitemap",
+            id: 1,
+            name: "Test",
+            referenceMark: [],
+            version: 2
+        }
+
         return (
             <Modal.Body>
                 <Search
@@ -46,10 +149,20 @@ class SearchFundsForm extends AbstractReactComponent {
                     placeholder={i18n('search.input.search')}
                     value={this.props.searchText}
                 />
-                <div> {/*TODO ELZA-1656: opřít o výsledky hledání, nezobrazovat pokud není co k zobrazení */}
-                    {i18n('arr.fund.search.result.count', this.props.count)}
-                    {/*TODO ELZA-1656: přidat komponentu pro zobrazení výsledků */}
-                </div>
+                {data.length > 0 ?
+                    <div className={"fund-search-result"}>
+                        {i18n('arr.fund.search.result.count', this.props.count)}
+                        {this.props.coun > 0 &&
+                            <div className="result-list">
+                                {this.renderNode(node)}
+                            </div>
+                        }
+                    </div>
+                    : 
+                    <div className="fund-search-no-fulltext">
+                        {i18n('arr.fund.search.noFulltext')}
+                    </div>
+                }
             </Modal.Body>
         )
     }
