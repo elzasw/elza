3f9bb00749b402b5a9ed654b35759bbcfd5cd4e6
David Slabý <xslaby@gmail.com>
2019-02-26 13:37:22 +0100

    ELZA-1680: Akordeon translation

--- elza-web/src/main/resources/static/res/js/messages_cs.js
+++ elza-web/src/main/resources/static/res/js/messages_cs.js
@@ -501,6 +501,7 @@ var messages = {
     "arr.fund.settings.panel.center.children": "Seznam potomků jednotky popisu",
     "arr.fund.settings.panel.rightPanel": "Pravý panel",
     "arr.fund.settings.panel.treeColorCoding": "Barevné označení úrovní ve stromu",
+    "arr.fund.settings.panel.acordeon": "Akordeon",
     "arr.fund.settings.panel.right.title": "Záložky v pravém panelu",
 
     "arr.fund.version": "Verze {0}",
