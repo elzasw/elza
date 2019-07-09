ceeca221253d819a3bfd9d382bb4100fe90909d7
Jiří Vaněk <vanek@marbes.cz>
2019-07-09 15:59:33 +0200

    WS

--- dspace-elza/src/main/java/cz/tacr/elza/destructransferrequest/service/WebServiceConfig.java
+++ dspace-elza/src/main/java/cz/tacr/elza/destructransferrequest/service/WebServiceConfig.java
@@ -2,6 +2,8 @@ package cz.tacr.elza.destructransferrequest.service;
 
 import javax.xml.ws.Endpoint;
 
+import org.dspace.services.ConfigurationService;
+import org.dspace.services.factory.DSpaceServicesFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +14,15 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource({"classpath:META-INF/cxf/cxf.xml", "classpath:META-INF/cxf/cxf-servlet.xml"})
 public class WebServiceConfig {
 
+    private ConfigurationService configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
+
     @Autowired
     private DaoRequestsImpl daoRequests;
 
     @Bean
     public Endpoint daoCoreServiceWS() {
-        return Endpoint.publish("http://localhost:8080/ws/DaoCoreRequests", daoRequests);
+        String baseUrl = configurationService.getProperty("dspace.baseUrl");
+
+        return Endpoint.publish(baseUrl + "/ws/DaoCoreRequests", daoRequests);
     }
 }
