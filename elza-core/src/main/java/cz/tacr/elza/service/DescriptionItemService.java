210e4fa53330e390874198c64961c464ec3a4f77
Petr Pytelka <petr.pytelka@lightcomp.cz>
2019-10-14 09:50:38 +0200

    Oprava zpusobu ulozeni zmenene hodnoty pri operaci Replace

--- elza-core/src/main/java/cz/tacr/elza/service/DescriptionItemService.java
+++ elza-core/src/main/java/cz/tacr/elza/service/DescriptionItemService.java
@@ -1564,7 +1564,7 @@ public class DescriptionItemService implements SearchIndexSupport<ArrDescItem> {
 
 		ArrDescItem descItemNew = prepareNewDescItem(descItem, dataNew, change);
 		descItemNew = descItemRepository.save(descItemNew);
-        arrangementCacheService.changeDescItem(descItem.getNodeId(), descItem, false);
+        arrangementCacheService.changeDescItem(descItem.getNodeId(), descItemNew, false);
     }
 
 
