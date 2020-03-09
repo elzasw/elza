c0f17d2b319ec11409843ea64840c54fe8799175
Jaroslav Todt <jaroslav.todt@lightcomp.cz>
2020-03-09 20:14:10 +0100

    pridani prihlasovani skrze SSO header

---
+++ elza/elza-core/src/main/java/cz/tacr/elza/security/ssoheader/SsoHeaderProperties.java
@@ -0,0 +1,25 @@
+package cz.tacr.elza.security.ssoheader;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@Component
+@ConditionalOnProperty(prefix = "elza.security.sso-header", name= "user-header")
+@ConfigurationProperties(prefix = "elza.security.sso-header", ignoreUnknownFields = false)
+public class SsoHeaderProperties {
+
+	@NotEmpty
+	private String userHeader;
+
+	public String getUserHeader() {
+		return userHeader;
+	}
+
+	public void setUserHeader(String userHeader) {
+		this.userHeader = userHeader;
+	}
+}
