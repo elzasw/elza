c217b7a5374c4e35c7b86b2eafdd02dafd2b1056
Petr Pytelka <petr.pytelka@lightcomp.cz>
2020-05-21 01:00:14 +0200

    Vymazani pozadavku na prepocet docasnych strukturovanych objektu pri startu aplikace

--- elza/elza-core/src/main/java/cz/tacr/elza/repository/SobjVrequestRepository.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/repository/SobjVrequestRepository.java
@@ -1,9 +1,12 @@
 package cz.tacr.elza.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import cz.tacr.elza.domain.ArrSobjVrequest;
+import cz.tacr.elza.domain.ArrStructuredObject;
 
 /**
  * Repository for ArrSobjVrequest
@@ -14,5 +17,15 @@ import cz.tacr.elza.domain.ArrSobjVrequest;
 public interface SobjVrequestRepository extends JpaRepository<ArrSobjVrequest, Integer> {
 
     int deleteByStructuredObjectFundId(Integer fundId);
+
+    @Modifying
+    @Query("DELETE FROM " + ArrSobjVrequest.TABLE_NAME + " v WHERE v."
+            + ArrSobjVrequest.FIELD_STRUCTURED_OBJECT_ID
+            + " IN ("
+            + "SELECT sd." + ArrStructuredObject.FIELD_STRUCTURED_OBJECT_ID + " FROM " + ArrStructuredObject.TABLE_NAME
+            + " sd WHERE sd." + ArrStructuredObject.FIELD_STATE
+            + " = 'TEMP'"
+            + ")")
+    void deleteByStructuredObjectStateTemp();
 
 }
