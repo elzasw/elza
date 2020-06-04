baf6a63118634e120761f554e9ab6a5808d23ace
Petr Compel <compel@marbes.cz>
2020-06-04 16:04:17 +0200

    React - Oprava RF wrapperu pro Autocomplete a ostatní

--- elza/elza-react/src/components/shared/form/ReduxFormFieldErrorDecorator.tsx
+++ elza/elza-react/src/components/shared/form/ReduxFormFieldErrorDecorator.tsx
@@ -1,23 +1,33 @@
-import React from "react";
-import {WrappedFieldProps} from "redux-form";
+import React from 'react';
+import {WrappedFieldProps} from 'redux-form';
 
 type Props = WrappedFieldProps & {
-    renderComponent: React.ComponentType
+    renderComponent: React.ComponentType;
     passOnly: boolean;
-}
+};
 
 /**
  * @param renderComponent Komponenta, ktera se ma zaobalit
  * @param passOnly Pouze preda meta hodnoty fieldu do podrizene komponenty, ale neresi zobrazovani chyb - pouziti napr. u Autocomplete
  */
 const ReduxFormFieldErrorDecorator = React.memo((props: Props) => {
-    const {renderComponent, meta: {touched, error}, ...other} = props;
-    const passOnly = props.passOnly;
-    return <>
-        {React.createElement(renderComponent, passOnly ? {...other, ...props.meta} : {...other} as any)}
-        {!passOnly && touched && error && <span className={'text-danger'}>{error}</span>}
-    </>;
-})
+    const {
+        renderComponent,
+        meta: {touched, error},
+        input,
+        passOnly,
+        ...other
+    } = props;
+
+    return (
+        <>
+            {React.createElement(
+                renderComponent,
+                passOnly ? {...other, ...input, ...props.meta} : ({...other, ...input} as any),
+            )}
+            {!passOnly && touched && error && <span className={'text-danger'}>{error}</span>}
+        </>
+    );
+});
 
 export default ReduxFormFieldErrorDecorator;
-
