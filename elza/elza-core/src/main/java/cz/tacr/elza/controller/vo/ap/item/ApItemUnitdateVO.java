928680567584f444e57b02c6f12235af026aa3aa
Petr Pytelka <petr.pytelka@lightcomp.cz>
2020-06-16 12:42:38 +0200

    Odstraneni duplicitniho kodu pri parsovani dataci

--- elza/elza-core/src/main/java/cz/tacr/elza/controller/vo/ap/item/ApItemUnitdateVO.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/controller/vo/ap/item/ApItemUnitdateVO.java
@@ -1,17 +1,14 @@
 package cz.tacr.elza.controller.vo.ap.item;
 
+import javax.persistence.EntityManager;
+
+import org.apache.commons.lang.Validate;
+
 import cz.tacr.elza.core.data.CalendarType;
-import cz.tacr.elza.core.data.DataType;
 import cz.tacr.elza.domain.ApItem;
 import cz.tacr.elza.domain.ArrData;
 import cz.tacr.elza.domain.ArrDataUnitdate;
-import cz.tacr.elza.domain.convertor.CalendarConverter;
 import cz.tacr.elza.domain.convertor.UnitDateConvertor;
-import org.apache.commons.lang.Validate;
-
-import javax.persistence.EntityManager;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 /**
  * @since 18.07.2018
@@ -58,38 +55,11 @@ public class ApItemUnitdateVO extends ApItemVO {
 
     @Override
     public ArrData createDataEntity(EntityManager em) {
-        ArrDataUnitdate data = new ArrDataUnitdate();
-
         // prepare calendar type
         CalendarType calType = CalendarType.fromId(calendarTypeId);
         Validate.notNull(calType);
-        data.setCalendarType(calType.getEntity());
 
-        UnitDateConvertor.convertToUnitDate(value, data);
-
-        // prepare normalized values - from
-        String valueFrom = data.getValueFrom();
-        Long normalizedFrom;
-        if (valueFrom != null) {
-            LocalDateTime locDateTime = LocalDateTime.parse(valueFrom, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-            normalizedFrom = CalendarConverter.toSeconds(calType, locDateTime);
-        } else {
-            normalizedFrom = Long.MIN_VALUE;
-        }
-        data.setNormalizedFrom(normalizedFrom);
-
-        // prepare normalized values - to
-        String valueTo = data.getValueTo();
-        Long normalizedTo;
-        if (valueTo != null) {
-            LocalDateTime locDateTime = LocalDateTime.parse(value, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-            normalizedTo = CalendarConverter.toSeconds(calType, locDateTime);
-        } else {
-            normalizedTo = Long.MAX_VALUE;
-        }
-        data.setNormalizedTo(normalizedTo);
-
-        data.setDataType(DataType.UNITDATE.getEntity());
+        ArrDataUnitdate data = ArrDataUnitdate.valueOf(calType, value);
         return data;
     }
 }
