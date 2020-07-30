3a866ed0a9db0f9b924c0d5043b7d76a35425325
Petr Pytelka <petr.pytelka@lightcomp.cz>
2020-07-31 00:59:14 +0200

    Uklid v AccessPointProcessor

--- elza/elza-core/src/main/java/cz/tacr/elza/dataexchange/input/aps/AccessPointProcessor.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/dataexchange/input/aps/AccessPointProcessor.java
@@ -1,23 +1,11 @@
 package cz.tacr.elza.dataexchange.input.aps;
 
-import cz.tacr.elza.core.data.ItemType;
-import cz.tacr.elza.dataexchange.input.DEImportException;
 import cz.tacr.elza.dataexchange.input.aps.context.AccessPointInfo;
 import cz.tacr.elza.dataexchange.input.context.ImportContext;
-import cz.tacr.elza.dataexchange.input.parts.PartProcessor;
-import cz.tacr.elza.dataexchange.input.parts.context.ItemWrapper;
-import cz.tacr.elza.dataexchange.input.parts.context.PartInfo;
-import cz.tacr.elza.dataexchange.input.parts.context.PartWrapper;
-import cz.tacr.elza.dbchangelog.DbChangeSet20200331164200;
-import cz.tacr.elza.domain.*;
-import cz.tacr.elza.exception.SystemException;
-import cz.tacr.elza.exception.codes.BaseCode;
-import cz.tacr.elza.schema.v2.*;
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import cz.tacr.elza.domain.ApPart;
+import cz.tacr.elza.domain.ApStateEnum;
+import cz.tacr.elza.domain.RulPartType;
+import cz.tacr.elza.schema.v2.AccessPoint;
 
 /**
  * Processing access points. Implementation is not thread-safe.
@@ -36,12 +24,7 @@ public class AccessPointProcessor extends FragmentsProcessor {
     }
 
     protected ApPart createPart(RulPartType type, AccessPointInfo apInfo) {
-        ApPart entity;
-        try {
-            entity = new ApPart();
-        } catch (Exception e) {
-            throw new SystemException("Failed to intialized no arg constructor, entity: " + ApPart.class, e);
-        }
+        ApPart entity = new ApPart();
         entity.setPartType(type);
         entity.setCreateChange(context.getCreateChange());
         entity.setState(ApStateEnum.OK);
