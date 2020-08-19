e0536699331faa687417a99cc82bb84c77235320
Martin Šlapa <slapa@marbes.cz>
2020-08-19 12:14:07 +0200

    MCV-65601 - Chybné URL pro statické zdroje za reverzní proxy

--- elza/elza-react/src/public_entry.jsx
+++ elza/elza-react/src/public_entry.jsx
@@ -1,1 +1,7 @@
-__webpack_public_path__ = serverContextPath + '/res/js/';
+/*eslint-disable */
+if (!__DEV__ && serverContextPath != null) {
+    __webpack_public_path__ = serverContextPath + '/';
+} else if (__DEV__) {
+    __webpack_public_path__ = 'http://localhost:3000/'
+}
+/*eslint-enable */
