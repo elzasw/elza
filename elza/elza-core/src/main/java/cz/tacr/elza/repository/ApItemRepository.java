30b86f7dc9f75ddc3791f6d18b14badfc1d29a6e
Petr Pytelka <petr.pytelka@lightcomp.cz>
2020-08-29 05:57:12 +0200

    Optimalizace dotazu na AP

--- elza/elza-core/src/main/java/cz/tacr/elza/repository/ApItemRepository.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/repository/ApItemRepository.java
@@ -1,13 +1,18 @@
 package cz.tacr.elza.repository;
 
-import cz.tacr.elza.domain.*;
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.List;
+import cz.tacr.elza.domain.ApAccessPoint;
+import cz.tacr.elza.domain.ApItem;
+import cz.tacr.elza.domain.ApPart;
+import cz.tacr.elza.domain.ArrDataRecordRef;
+import cz.tacr.elza.domain.RulItemType;
 
 @Repository
 public interface ApItemRepository extends JpaRepository<ApItem, Integer> {
@@ -24,16 +29,16 @@ public interface ApItemRepository extends JpaRepository<ApItem, Integer> {
     @Query("SELECT i FROM ApItem i LEFT JOIN FETCH i.data d WHERE i.deleteChange IS NULL AND i.part.partId = :partId")
     List<ApItem> findValidItemsByPartId(@Param("partId") Integer partId);
 
-    @Query("SELECT i FROM ApItem i LEFT JOIN FETCH i.data d  JOIN FETCH i.part p WHERE i.deleteChange IS NULL AND p.accessPoint = :accessPoint")
+    @Query("SELECT i FROM ApItem i JOIN FETCH i.part p LEFT JOIN FETCH i.data d  WHERE i.deleteChange IS NULL AND p.accessPoint = :accessPoint")
     List<ApItem> findValidItemsByAccessPoint(@Param("accessPoint") ApAccessPoint accessPoint);
 
-    @Query("SELECT i FROM ApItem i LEFT JOIN FETCH i.data d  JOIN FETCH i.part p WHERE i.deleteChange IS NULL AND p.accessPoint = :accessPoint AND i.createChange.changeId > :changeId")
+    @Query("SELECT i FROM ApItem i JOIN FETCH i.part p LEFT JOIN FETCH i.data d WHERE i.deleteChange IS NULL AND p.accessPoint = :accessPoint AND i.createChange.changeId > :changeId")
     List<ApItem> findNewerValidItemsByAccessPoint(@Param("accessPoint") ApAccessPoint accessPoint, @Param("changeId") Integer changeId);
 
-    @Query("SELECT i FROM ApItem i LEFT JOIN FETCH i.data d  JOIN FETCH i.part p WHERE i.deleteChange IS NULL AND p.accessPoint IN :accessPoints")
+    @Query("SELECT i FROM ApItem i JOIN FETCH i.part p LEFT JOIN FETCH i.data d WHERE i.deleteChange IS NULL AND p.accessPoint IN :accessPoints")
     List<ApItem> findValidItemsByAccessPoints(@Param("accessPoints") Collection<ApAccessPoint> accessPoints);
 
-    @Query("SELECT i FROM ApItem i LEFT JOIN FETCH i.data d  JOIN FETCH i.part p JOIN FETCH i.itemType it WHERE i.deleteChange IS NULL AND p.accessPoint = :accessPoint")
+    @Query("SELECT i FROM ApItem i  JOIN FETCH i.part p LEFT JOIN FETCH i.data d JOIN FETCH i.itemType it WHERE i.deleteChange IS NULL AND p.accessPoint = :accessPoint")
     List<ApItem> findValidItemsByAccessPointMultiFetch(@Param("accessPoint") ApAccessPoint accessPoint);
 
     @Query("SELECT i FROM ApItem i JOIN i.part p JOIN p.partType pt LEFT JOIN FETCH i.data WHERE i.deleteChange IS NULL AND i.itemType = :itemType AND p.deleteChange IS NULL AND pt.code = :partTypeCode AND p.accessPointId = :accessPointId")
