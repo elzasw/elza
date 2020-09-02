9f7cbac65ef04c06e04e8b4e67e0e0f4f247c0f6
Petr Pytelka <petr.pytelka@lightcomp.cz>
2020-09-02 14:57:26 +0200

    Vystupy: moznost seskupeni shodnych specifikaci

--- elza/elza-core/src/main/java/cz/tacr/elza/print/format/ValueFormatter.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/print/format/ValueFormatter.java
@@ -1,8 +1,9 @@
 package cz.tacr.elza.print.format;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import org.springframework.util.StringUtils;
+import java.util.Map;
 
 import cz.tacr.elza.print.item.Item;
 import cz.tacr.elza.print.item.ItemSpec;
@@ -22,33 +23,39 @@ public class ValueFormatter implements FormatAction {
 
     @Override
     public void format(FormatContext ctx, List<Item> items) {
+
+        // write value with specification
+        SpecTitleSource specTitleSource = ctx.getSpecTitleSource();
+
+        List<String> specs = new ArrayList<>();
+        Map<String, List<String>> specValues = new HashMap<>();
+
         for (Item item : items) {
             if (item.getType().getCode().equals(itemType)) {
-                formatItem(ctx, item);
+
+                String value = item.getSerializedValue();
+                ItemSpec spec = item.getSpecification();
+                if (spec == null) {
+                    // write value without specification
+                    ctx.appendValue(value);
+                } else {
+                    // store spec value for later processing
+                    String specName = specTitleSource.getValue(spec).toLowerCase();
+                    if (ctx.getGroupBySpec()) {
+                        specs.add(specName);
+                        List<String> values = specValues.computeIfAbsent(specName, a -> new ArrayList<>());
+                        values.add(value);
+                    } else {
+                        ctx.appendSpecWithValue(specName, value);
+                    }
+                }
             }
         }
 
-    }
-
-    /**
-     * Format one item
-     *
-     * @param ctx
-     * @param item
-     */
-    private void formatItem(FormatContext ctx, Item item) {
-        String value = item.getSerializedValue();
-
-        ItemSpec spec = item.getSpecification();
-        if (spec != null) {
-            // write value with specification
-        	SpecTitleSource specTitleSource = ctx.getSpecTitleSource();
-        	String specName = specTitleSource.getValue(spec).toLowerCase();
-
-            ctx.appendSpecWithValue(specName, value);
-        } else {
-            // write value without specification
-            ctx.appendValue(value);
+        // process values with specification
+        for (String specName : specs) {
+            List<String> values = specValues.get(specName);
+            ctx.appendSpecWithValues(specName, values);
         }
     }
 }
