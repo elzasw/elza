ce26f9ae7224fbfe15c936f30163f89fef208cba
Petr Pytelka <petr.pytelka@lightcomp.cz>
2021-01-20 12:14:53 +0100

    Oprava exportu chybejicich vztahu

--- elza/elza-core/src/main/java/cz/tacr/elza/dataexchange/output/items/APRefConvertor.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/dataexchange/output/items/APRefConvertor.java
@@ -1,21 +1,45 @@
 package cz.tacr.elza.dataexchange.output.items;
 
-import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import cz.tacr.elza.domain.ApBinding;
 import cz.tacr.elza.domain.ArrData;
 import cz.tacr.elza.domain.ArrDataRecordRef;
+import cz.tacr.elza.exception.SystemException;
 import cz.tacr.elza.schema.v2.DescriptionItemAPRef;
 import cz.tacr.elza.schema.v2.ObjectFactory;
 
 public class APRefConvertor implements ItemDataConvertor {
 
+    private static final Logger log = LoggerFactory.getLogger(APRefConvertor.class);
+
     @Override
     public DescriptionItemAPRef convert(ArrData data, ObjectFactory objectFactory) {
-        Validate.isTrue(data.getClass() == ArrDataRecordRef.class, "Invalid data type, dataId:", data.getDataId());
+        if (data == null) {
+            log.error("data is null");
+            throw new SystemException("Data is null");
+        }
+        if (!(data instanceof ArrDataRecordRef)) {
+            log.error("data is null, dataId: ", data.getDataId());
+            throw new SystemException("Invalid data type, dataId: " + data.getDataId())
+                    .set("dataId", data.getDataId());
+        }
 
         ArrDataRecordRef apRef = (ArrDataRecordRef) data;
+
         DescriptionItemAPRef item = objectFactory.createDescriptionItemAPRef();
-        item.setApid(apRef.getRecordId().toString());
+        if (apRef.getRecordId() == null) {
+            ApBinding binding = apRef.getBinding();
+            if (binding == null) {
+                log.error("ArrDataRecordRef without connected recordId and binding, dataId: ", data.getDataId());
+                throw new SystemException("ArrDataRecordRef without connected recordId and binding")
+                        .set("dataId", data.getDataId());
+            }
+            item.setApid(binding.getValue());
+        } else {
+            item.setApid(apRef.getRecordId().toString());
+        }
         return item;
     }
 }
