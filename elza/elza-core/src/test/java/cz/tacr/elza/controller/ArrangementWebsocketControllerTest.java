005db7ce5526c0ef2f3711866051f2463b6d0c13
Sergey Iryupin <sergey.iryupin@lightcomp.cz>
2021-03-11 14:16:17 +0100

    Elza 6967 test pro zalozeni vice uzlu jednotek popisu

--- elza/elza-core/src/test/java/cz/tacr/elza/controller/ArrangementWebsocketControllerTest.java
+++ elza/elza-core/src/test/java/cz/tacr/elza/controller/ArrangementWebsocketControllerTest.java
@@ -1,40 +1,27 @@
 package cz.tacr.elza.controller;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
-import javax.transaction.Transactional;
-
-import org.apache.commons.lang3.concurrent.AtomicInitializer;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.task.TaskSchedulerBuilder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompFrameHandler;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSession.Receiptable;
 import org.springframework.messaging.simp.stomp.StompSessionHandler;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.client.WebSocketClient;
@@ -42,21 +29,18 @@ import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 import cz.tacr.elza.controller.vo.AddLevelParam;
+import cz.tacr.elza.controller.vo.ArrFundVersionVO;
+import cz.tacr.elza.controller.vo.TreeData;
+import cz.tacr.elza.controller.vo.TreeNodeVO;
 import cz.tacr.elza.controller.vo.nodes.ArrNodeVO;
-import cz.tacr.elza.domain.ArrFund;
 import cz.tacr.elza.domain.ArrNode;
-import cz.tacr.elza.security.UserDetail;
-import cz.tacr.elza.service.DaoService;
 import cz.tacr.elza.service.FundLevelService;
-import cz.tacr.elza.service.UserService;
 import cz.tacr.elza.test.ApiException;
 import cz.tacr.elza.test.controller.vo.Fund;
 
 public class ArrangementWebsocketControllerTest extends AbstractControllerTest {
 
     static private Logger logger = LoggerFactory.getLogger(ArrangementWebsocketControllerTest.class);
-
-    AddLevelParam addLevelParam;
 
     enum ReceiptStatus {
         RCP_WAITING,
@@ -68,25 +52,35 @@ public class ArrangementWebsocketControllerTest extends AbstractControllerTest {
     public void addLevelTest() throws ApiException, InterruptedException, ExecutionException, IllegalAccessException {
         MyStompSessionHandler sessionHandler = new MyStompSessionHandler();
         StompSession session = connectWebSocketStompClient(sessionHandler);
-        
+
         FieldUtils.writeField(StompCommand.RECEIPT, "body", true, true);
 
-        session.subscribe("/topic/api/changes", sessionHandler);
-        
+        //session.subscribe("/topic/api/changes", sessionHandler); // funguje i bez tohoto řádku
+
         Fund fund = createFund("Jmeno", "kod");
-        List<ArrNode> nodes = nodeRepository.findAll();
+        helperTestService.waitForWorkers();
+        ArrFundVersionVO fundVersion = getOpenVersion(fund);
 
-        ArrNode node = nodes.get(0);
+        ArrangementController.FaTreeParam input = new ArrangementController.FaTreeParam();
+        input.setVersionId(fundVersion.getId());
+        TreeData treeData = getFundTree(input);
 
-        ArrNodeVO staticNode = ArrNodeVO.newInstance(node);
-        ArrNodeVO staticNodeParent = ArrNodeVO.newInstance(node);
+        // Musí existovat root node
+        assertNotNull(treeData.getNodes());
 
+        // Musí existovat pouze root node
+        assertTrue(treeData.getNodes().size() == 1);
+
+        TreeNodeVO rootTreeNodeClient = treeData.getNodes().iterator().next();
+        ArrNodeVO rootNode = convertTreeNode(rootTreeNodeClient);
+
+        // Příprava objektu s daty pro odeslání
         AddLevelParam addLevelParam = new AddLevelParam();
-        addLevelParam.setVersionId(fund.getId());
-        addLevelParam.setStaticNode(staticNode);
-        addLevelParam.setStaticNodeParent(staticNodeParent);
+        addLevelParam.setVersionId(fundVersion.getId());
+        addLevelParam.setStaticNode(rootNode);
+        addLevelParam.setStaticNodeParent(rootNode);
         addLevelParam.setDirection(FundLevelService.AddLevelDirection.CHILD);
-        //addLevelParam.setCount(2);
+        addLevelParam.setCount(2); // přidat více než 1 úroveň
 
         session.setAutoReceipt(true);
         Receiptable receipt = session.send("/app/arrangement/levels/add", addLevelParam);
@@ -105,22 +99,25 @@ public class ArrangementWebsocketControllerTest extends AbstractControllerTest {
             Thread.sleep(100);
         }
         assertEquals(ReceiptStatus.RCP_RECEIVED, receiptStatus.get());
-        
-        nodes = nodeRepository.findAll();
-        assertTrue(nodes.size() == 2);
-        
+
+        // Monitorování výsledků dotazu
+        List<ArrNode> nodes = nodeRepository.findAll();
+        assertTrue(nodes.size() == 3);
+
         session.disconnect();
     }
 
     private StompSession connectWebSocketStompClient(StompSessionHandler sessionHandler) throws InterruptedException, ExecutionException {
         WebSocketClient client = new StandardWebSocketClient();
         WebSocketStompClient stompClient = new WebSocketStompClient(client);
+
         ThreadPoolTaskScheduler taskScheduler = new TaskSchedulerBuilder().poolSize(1).build();
         taskScheduler.initialize();
         stompClient.setTaskScheduler(taskScheduler);
 
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
 
+        // Dostáváme údaje k autorizaci
         WebSocketHttpHeaders handshakeHeaders = new WebSocketHttpHeaders();
         StringBuilder cookie = new StringBuilder();
         cookies.forEach((name, value) -> {
@@ -132,7 +129,7 @@ public class ArrangementWebsocketControllerTest extends AbstractControllerTest {
         if (cookie.length() > 0) {
             handshakeHeaders.set(HttpHeaders.COOKIE, cookie.toString());
         }
-        
+
         ListenableFuture<StompSession> futureSession = stompClient.connect("ws://localhost:" + port + "/stomp", 
                                                                            handshakeHeaders, sessionHandler);
         return futureSession.get();
@@ -141,7 +138,7 @@ public class ArrangementWebsocketControllerTest extends AbstractControllerTest {
     class MyStompSessionHandler implements StompSessionHandler {
 
         private Logger logger = LoggerFactory.getLogger(this.getClass());
-        
+
         @Override
         public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
             logger.info("New session established : " + session.getSessionId());
