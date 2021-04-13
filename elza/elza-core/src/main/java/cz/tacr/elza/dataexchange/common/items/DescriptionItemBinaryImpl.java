4faf2c09a531dd3ebaf54560849b5003c2c70688
Petr Pytelka <petr.pytelka@lightcomp.cz>
2021-04-13 17:14:45 +0200

    DescriptionItemBinary - priprava

---
+++ elza/elza-core/src/main/java/cz/tacr/elza/dataexchange/common/items/DescriptionItemBinaryImpl.java
@@ -0,0 +1,7 @@
+package cz.tacr.elza.dataexchange.common.items;
+
+import cz.tacr.elza.schema.v2.DescriptionItemBinary;
+
+public class DescriptionItemBinaryImpl extends DescriptionItemBinary {
+
+}
