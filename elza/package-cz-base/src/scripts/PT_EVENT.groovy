4d411eb766c2a123bccfd4dfe8655fb8e86fbf5f
Petr Pytelka <petr.pytelka@lightcomp.cz>
2021-08-27 16:19:28 +0200

    Arch.entity - nepropagovani casti z podrizenych partu do nadrizeneho popisu u udalosti

--- elza/package-cz-base/src/scripts/PT_EVENT.groovy
+++ elza/package-cz-base/src/scripts/PT_EVENT.groovy
@@ -9,24 +9,24 @@ return generate(PART)
 
 static GroovyResult generate(final GroovyPart part) {
 
+    GroovyAppender base = GroovyUtils.createAppender(part)
+    base.add("EV_TYPE")
+    base.add("EV_BEGIN").withSeparator(", ").withPrefix("od: ")
+    base.add("EV_END").withSeparator(", ").withPrefix("do: ")
+    GroovyResult result = new GroovyResult()
+    result.setDisplayName(base.build())
+
     def children = part.getChildren()
     GroovyAppender rels = GroovyUtils.createAppender(part)
-    // Nepropagovani casti z podrizenych partu
-    /*if (children != null) {
+    if (children != null) {
         for (GroovyPart childPart : children) {
             GroovyAppender childBase = GroovyUtils.createAppender(childPart)
             childBase.add("REL_ENTITY").withSpec()
             rels.addStr(childBase.build()).withSeparator(", ")
         }
-    }*/
-
-    GroovyAppender base = GroovyUtils.createAppender(part)
-    base.add("EV_TYPE")
-    base.add("EV_BEGIN").withSeparator(", ").withPrefix("od: ")
-    base.add("EV_END").withSeparator(", ").withPrefix("do: ")
+    }
     base.addStr(rels.build()).withSeparator(" ").withPrefix("(").withPostfix(")")
+    result.addIndex("DISPLAY_NAME_LOWER", base.build())
 
-    GroovyResult result = new GroovyResult()
-    result.setDisplayName(base.build())
     return result
 }
