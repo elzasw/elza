6e03c38accfea43624fc3f084dc56d9eee2d7afb
Petr Pytelka <petr.pytelka@lightcomp.cz>
2021-10-01 02:35:48 +0200

    #7452 - Tabulkové zobrazení - filtr PP datace

--- elza/elza-core/src/main/java/cz/tacr/elza/filter/condition/IntersectDescItemCondition.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/filter/condition/IntersectDescItemCondition.java
@@ -1,5 +1,6 @@
 package cz.tacr.elza.filter.condition;
 
+import org.apache.commons.lang3.Validate;
 import org.apache.lucene.search.Query;
 import org.hibernate.search.query.dsl.QueryBuilder;
 import org.springframework.util.Assert;
@@ -18,22 +19,17 @@ public class IntersectDescItemCondition<T extends Interval<IV>, IV> extends Abst
 
     @Override
     public Query createLuceneQuery(QueryBuilder queryBuilder) {
-        Assert.notNull(queryBuilder);
+        Validate.notNull(queryBuilder);
 
         Interval<IV> interval = getValue();
         IV from = interval.getFrom();
         IV to = interval.getTo();
 
-        // conditionFrom < dataceFrom && conditionTo > dataceFrom && conditionTo < dataceTo
-        Query fromQuery1 = queryBuilder.range().onField(getAttributeNameFrom()).from(from).to(to).createQuery();
-        Query fromQuery2 = queryBuilder.range().onField(getAttributeNameTo()).above(to).createQuery();
-        Query fromQuery = queryBuilder.bool().must(fromQuery1).must(fromQuery2).createQuery();
-
-        //conditionFrom < dataceTo && conditionTo > dataceTo && conditionFrom > dataceFrom
-        Query toQuery1 = queryBuilder.range().onField(getAttributeNameTo()).from(from).to(to).createQuery();
-        Query toQuery2 = queryBuilder.range().onField(getAttributeNameFrom()).below(from).createQuery();
-        Query toQuery = queryBuilder.bool().must(toQuery1).must(toQuery2).createQuery();
-
-        return queryBuilder.bool().should(fromQuery).should(toQuery).createQuery();
+        // (StartA <= EndB) and (EndA >= StartB)
+        // see https://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+        Query fromQuery1 = queryBuilder.range().onField(getAttributeNameFrom()).below(to).createQuery();
+        Query fromQuery2 = queryBuilder.range().onField(getAttributeNameTo()).above(from).createQuery();
+        
+        return queryBuilder.bool().must(fromQuery1).must(fromQuery2).createQuery();
     }
 }
