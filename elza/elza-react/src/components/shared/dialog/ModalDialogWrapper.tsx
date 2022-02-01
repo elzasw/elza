b15cf87eeef73ce3b3ca9931ab138844a9720c4f
Jakub Randák <jakub.randak@lightcomp.cz>
2022-02-01 11:07:24 +0100

    elza-react - predelani ModalDialogWrapperu do typescriptu, pridani moznosti zavreni dialogu pri kliknuti mimo nej

---
+++ elza/elza-react/src/components/shared/dialog/ModalDialogWrapper.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {FC, useEffect, useRef} from 'react';
+import ReactDOM from 'react-dom';
+import {Modal} from 'react-bootstrap';
+import {setInputFocus} from 'components/Utils.jsx';
+
+/**
+ * Obal modálního dialogu
+ */
+export interface ModalDialogWraperProps {
+    className: string;
+    title?: string;
+    onHide?: () => void;
+    closeOnClickOutside?: boolean;
+}
+
+export const ModalDialogWrapper:FC<ModalDialogWraperProps> = ({
+    title,
+    className,
+    children,
+    onHide = () => {return;},
+    closeOnClickOutside = false,
+}) => {
+    const modalBody = useRef<HTMLDivElement>(null);
+    const hide = useRef(false);
+
+    useEffect(()=>{
+        if (modalBody) {
+            const el = ReactDOM.findDOMNode(modalBody.current);
+            if (el) {
+                setInputFocus(el, false);
+            }
+        }
+    },[])
+
+    const handleHide = () => {
+        if (!hide.current) {
+            onHide && onHide();
+            hide.current = true;
+        }
+    };
+
+    return (
+        <Modal
+            backdrop={closeOnClickOutside ? undefined : 'static'}
+            className={className}
+            show={true}
+            onHide={handleHide}
+            maskClosable={false}
+        >
+            {title !== null && (
+                <Modal.Header closeButton>
+                    <Modal.Title>{title}</Modal.Title>
+                </Modal.Header>
+            )}
+
+            <div ref={modalBody} className="modal-body-container">
+                {children}
+            </div>
+        </Modal>
+    );
+}
