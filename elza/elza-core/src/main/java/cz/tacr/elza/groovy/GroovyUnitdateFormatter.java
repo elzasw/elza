187022c604f06442a0923772788bde1037ef6f5d
Sergey Iryupin <sergey.iryupin@lightcomp.cz>
2022-03-25 09:10:19 +0100

    Elza 7661 chronologické doplňk, oprava po ověření

--- elza/elza-core/src/main/java/cz/tacr/elza/groovy/GroovyUnitdateFormatter.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/groovy/GroovyUnitdateFormatter.java
@@ -108,6 +108,10 @@ public class GroovyUnitdateFormatter {
         return prefix + str;
     }
 
+    private String getFirstWord(String str) {
+        return str != null? str.split(" ")[0] : null;
+    }
+
     @NotNull
     public String build() {
         String result = "";
@@ -116,7 +120,7 @@ public class GroovyUnitdateFormatter {
 
         if (from == null && to == null) {
             return result;
-        } else if (from != null && to != null && formatYear && yearEqual && fromStr.equals(toStr) && prefixFrom.equals(prefixTo)) {
+        } else if (from != null && to != null && formatYear && yearEqual && fromStr.equals(toStr) && getFirstWord(prefixFrom).equals(getFirstWord(prefixTo))) {
             result = completeEqualUnitdate(from.getUnitdateValue(), to.getUnitdateValue(), fromStr);
         } else if (from != null && to != null) {
             result = completeBeginUnitdate(from.getUnitdateValue(), fromStr) + "-" + completeEndUnitdate(to.getUnitdateValue(), toStr);
