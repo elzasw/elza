0af5acd1708d35b5f9b1a18c6c47c873aa667ea1
Petr Pytelka <petr.pytelka@lightcomp.cz>
2022-08-18 17:22:26 +0200

    Oprava kontroly mist uziti v pripade velkeho poctu

--- elza/elza-core/src/main/java/cz/tacr/elza/repository/DescItemRepository.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/repository/DescItemRepository.java
@@ -1,14 +1,23 @@
 package cz.tacr.elza.repository;
 
-import cz.tacr.elza.domain.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import cz.tacr.elza.domain.ApAccessPoint;
+import cz.tacr.elza.domain.ArrChange;
+import cz.tacr.elza.domain.ArrData;
+import cz.tacr.elza.domain.ArrDescItem;
+import cz.tacr.elza.domain.ArrFile;
+import cz.tacr.elza.domain.ArrFund;
+import cz.tacr.elza.domain.ArrNode;
+import cz.tacr.elza.domain.RulItemSpec;
+import cz.tacr.elza.domain.RulItemType;
 
 
 /**
@@ -210,7 +219,7 @@ public interface DescItemRepository extends ElzaJpaRepository<ArrDescItem, Integ
     List<ArrDescItem> findByNodeAndDeleteChangeIsNullAndItemTypeAndSpecItemTypeIsNull(ArrNode node, RulItemType itemType);
 
     @Query("SELECT n.fundId, i.nodeId, d.dataId FROM arr_desc_item i JOIN i.data d JOIN i.node n WHERE i.deleteChange IS NULL and i.data in (?1)")
-    List<Object[]> findFundIdNodeIdDataIdByDataAndDeleteChangeIsNull(List<? extends ArrData> data);
+    List<Object[]> findFundIdNodeIdDataIdByDataAndDeleteChangeIsNull(Collection<? extends ArrData> data);
 
     @Query("Select i from arr_desc_item i join arr_data_record_ref d on i.data = d WHERE d.record = :record AND i.deleteChange IS NULL")
     List<ArrDescItem> findArrItemByRecord(@Param("record") final ApAccessPoint record);
