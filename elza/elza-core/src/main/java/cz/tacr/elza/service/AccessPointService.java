c15a0e27b6543393268da76603bc0adb0af741e8
Sergey Iryupin <biblelamp@gmail.com>
2022-12-16 12:50:33 +0100

    #8146 dodatečné logování

--- elza/elza-core/src/main/java/cz/tacr/elza/service/AccessPointService.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/service/AccessPointService.java
@@ -2719,6 +2719,8 @@ public class AccessPointService {
     @Transactional(TxType.MANDATORY)
     public void generateSync(final ApAccessPoint accessPoint, final ApPart apPart) {
         boolean successfulGeneration = updatePartValue(apPart);
+
+        logger.debug("Validate accessPointid={}, partId={}, successfulGeneration={}", accessPoint.getAccessPointId(), apPart.getPartId(), successfulGeneration);
         validate(accessPoint, successfulGeneration);
     }
 
@@ -2756,9 +2758,10 @@ public class AccessPointService {
 
         Integer prefPartId = accessPoint.getPreferredPartId();
         boolean successfulGeneration = updatePartValues(apState, prefPartId, partList, itemMap, async);
+
+        logger.debug("Validate accessPointid={}, partListSize={}, successfulGeneration={}", accessPoint.getAccessPointId(), partList.size(), successfulGeneration);
         return validate(accessPoint, successfulGeneration);
     }
-
 
     /**
      * Zapsání validačních chyb přístupového bodu do databáze.
@@ -2815,7 +2818,7 @@ public class AccessPointService {
         }
 
         logger.debug("Save accessPoint id={} version={}", accessPoint.getAccessPointId(), accessPoint.getVersion());
-        return apAccessPointRepository.save(accessPoint);
+        return apAccessPointRepository.saveAndFlush(accessPoint);
     }
 
     @Nullable
