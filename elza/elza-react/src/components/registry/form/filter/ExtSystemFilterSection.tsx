9e9c18e07163699cd6d16466f32009696a0a43c2
Jakub Randák <jakub.randak@lightcomp.cz>
2023-03-24 18:40:02 +0100

    elza-react - 8370 - Zápis entity do externího systému - upozornění, pokud odesílaný záznam obsahuje nesynchronizované související entity

--- elza/elza-react/src/components/registry/form/filter/ExtSystemFilterSection.tsx
+++ elza/elza-react/src/components/registry/form/filter/ExtSystemFilterSection.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Field, FormSection} from 'redux-form';
+import {Field as FinalField} from 'react-final-form';
 import i18n from "../../../i18n";
 import {FormInputField} from "../../../shared";
+import { ApExternalSystemSimpleVO } from 'typings/store';
 
 type OwnProps = {
     submitting: boolean;
@@ -12,6 +14,37 @@ type OwnProps = {
 }
 
 type Props = {} & OwnProps;
+
+interface FinalProps {
+    disabled: boolean;
+    name?: string; // název pro FormSection
+    sectionName?: string;
+    label?: string;
+    hideName?: boolean;
+    extSystems: ApExternalSystemSimpleVO[];
+}
+
+export const ExtSystemFilterSectionFinal = ({
+    disabled, 
+    name = "extSystem", 
+    label = i18n('ap.ext-search.ext-system'),
+    extSystems, 
+    sectionName = i18n('ap.ext-search.section.ext-systems'), 
+    hideName = false
+}: FinalProps) => {
+    return <div className="filter-section">
+        {!hideName && <span className="name-section">{sectionName}</span>}
+        <FinalField name={name}
+               label={label}
+               type="autocomplete"
+               component={FormInputField}
+               getItemId={(item:ApExternalSystemSimpleVO) => item && item.code}
+               useIdAsValue
+               items={extSystems}
+               disabled={disabled}
+        />
+    </div>
+};
 
 const ExtSystemFilterSection = ({submitting, nameFormSection = "", extSystems, name = 'ap.ext-search.section.ext-systems', hideName = false}: Props) => {
     return <FormSection name={nameFormSection} className="filter-section">
