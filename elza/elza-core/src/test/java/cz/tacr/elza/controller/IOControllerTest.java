c8f6387a37960c9eb67e3757cbd25ca1691cc404
Petr Pytelka <petr.pytelka@lightcomp.cz>
2023-08-23 03:38:05 +0200

    Oprava testu

--- elza/elza-core/src/test/java/cz/tacr/elza/controller/IOControllerTest.java
+++ elza/elza-core/src/test/java/cz/tacr/elza/controller/IOControllerTest.java
@@ -2,6 +2,7 @@ package cz.tacr.elza.controller;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 
@@ -10,6 +11,8 @@ import org.junit.Test;
 import cz.tacr.elza.controller.vo.ArrFundVersionVO;
 import cz.tacr.elza.test.ApiException;
 import cz.tacr.elza.test.controller.vo.ExportParams;
+import cz.tacr.elza.test.controller.vo.ExportRequestState;
+import cz.tacr.elza.test.controller.vo.ExportRequestStatus;
 import cz.tacr.elza.test.controller.vo.Fund;
 import cz.tacr.elza.test.controller.vo.FundSections;
 
@@ -28,17 +31,19 @@ public class IOControllerTest extends AbstractControllerTest {
         int requestId = ioApi.ioExportRequest(exportParams);
         assertEquals(requestId, 1);
 
-        while (true) {
-            try {
-                ioApi.ioGetExportStatus(requestId);
-                break;
-            } catch (ApiException ex) {
-                if (ex.getCode() != 102) {
-                    ex.printStackTrace();
-                    break;
-                }
-            }
+        ExportRequestStatus expStatus = null;
+        int counter = 0;
+        try {
+            do {
+                Thread.sleep(50);
+                expStatus = ioApi.ioGetExportStatus(requestId);
+                counter++;
+            } while (expStatus.getState() != ExportRequestState.FINISHED && counter < 1000);
+        } catch (Exception e) {
+            fail("Exception while waiting on result: " + e);
         }
+        assertNotNull(expStatus);
+        assertEquals(ExportRequestState.FINISHED, expStatus.getState());
 
         File file = ioApi.ioGetExportFile(requestId);
         assertNotNull(file);
