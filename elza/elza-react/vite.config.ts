f2cbed366a4ab6e09275515f079728fdaffdb147
Jakub Randák <jakub.randak@lightcomp.cz>
2024-01-29 11:57:32 +0100

    pridani nastaveni endpointu frontend dev serveru pres promennou prostredi

--- elza/elza-react/vite.config.ts
+++ elza/elza-react/vite.config.ts
@@ -1,53 +1,59 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "src": path.resolve(__dirname, "./src/"),
-      "stores": path.resolve(__dirname, "./src/stores/"),
-      "components": path.resolve(__dirname, "./src/components/"),
-      "actions": path.resolve(__dirname, "./src/actions/"),
-      "pages": path.resolve(__dirname, "./src/pages/"),
-      "api": path.resolve(__dirname, "./src/api/"),
-      "utils": path.resolve(__dirname, "./src/utils/"),
-      "typings": path.resolve(__dirname, "./src/typings/"),
-      "shared": path.resolve(__dirname, "./src/shared/"),
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "static/res/js/[name].js",
-        assetFileNames: "static/res/assets/[name][extname]",
-        chunkFileNames: "static/res/js/[name].js"
+
+const defaultEndpoint = 'http://localhost:8080';
+
+export default ({ mode }) => {
+
+  // Load environment variables
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") }
+  const endpoint = process.env.ENDPOINT || defaultEndpoint;
+
+  return defineConfig({
+    resolve: {
+      alias: {
+        "src": path.resolve(__dirname, "./src/"),
+        "stores": path.resolve(__dirname, "./src/stores/"),
+        "components": path.resolve(__dirname, "./src/components/"),
+        "actions": path.resolve(__dirname, "./src/actions/"),
+        "pages": path.resolve(__dirname, "./src/pages/"),
+        "api": path.resolve(__dirname, "./src/api/"),
+        "utils": path.resolve(__dirname, "./src/utils/"),
+        "typings": path.resolve(__dirname, "./src/typings/"),
+        "shared": path.resolve(__dirname, "./src/shared/"),
+        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
       }
-    }
-  },
-  server: {
-    port: 3000,
-    hmr: {
-      port: 3001
     },
-    proxy: {
-      // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
-      //'/api', '/login'
-      '/login': 'http://10.2.0.27:8081',
-      '/api': {
-        target: 'http://10.2.0.27:8081',
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''),
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: "static/res/js/[name].js",
+          assetFileNames: "static/res/assets/[name][extname]",
+          chunkFileNames: "static/res/js/[name].js"
+        }
+      }
+    },
+    server: {
+      port: 3000,
+      hmr: {
+        port: 3001
       },
-      '/stomp': {
-        target: 'http://10.2.0.27:8081',
-        changeOrigin: true,
-        ws: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''),
-      }
+      proxy: {
+        '/login': endpoint,
+        '/api': {
+          target: endpoint,
+          changeOrigin: true,
+        },
+        '/stomp': {
+          target: endpoint,
+          changeOrigin: true,
+          ws: true,
+        }
+      },
     },
-  },
-})
+  })
+}
