d14168c5bfb98b21cb870e6836d789cca4d79973
Petr Pytelka <petr.pytelka@lightcomp.cz>
2024-02-05 17:34:40 +0100

    #8847 - Nelze vygenerovat výstup v případě cyklických vazeb

--- elza/elza-core/src/main/java/cz/tacr/elza/print/item/convertors/UriRefItemConvertor.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/print/item/convertors/UriRefItemConvertor.java
@@ -4,7 +4,6 @@ import cz.tacr.elza.core.data.DataType;
 import cz.tacr.elza.domain.ArrDataUriRef;
 import cz.tacr.elza.domain.ArrNode;
 import cz.tacr.elza.domain.Item;
-import cz.tacr.elza.print.Node;
 import cz.tacr.elza.print.item.AbstractItem;
 import cz.tacr.elza.print.item.ItemType;
 import cz.tacr.elza.print.item.ItemUriRef;
@@ -18,11 +17,8 @@ public class UriRefItemConvertor extends AbstractItemConvertor {
         }
         ArrDataUriRef data = (ArrDataUriRef) item.getData();
         ArrNode linkedNode = data.getArrNode();
-        Node node = null;
-        if (linkedNode != null) {
-            node = context.getNode(data.getArrNode());
-        }
 
-        return new ItemUriRef(data.getSchema(), data.getUriRefValue(), data.getDescription(), node);
+        return new ItemUriRef(data.getSchema(), data.getUriRefValue(), data.getDescription(),
+                linkedNode, context);
     }
 }
