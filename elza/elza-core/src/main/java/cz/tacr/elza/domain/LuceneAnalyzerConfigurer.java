a47568f292072f1e2d1f95702e6d1fb3fc9738d1
Sergey Iryupin <biblelamp@gmail.com>
2024-03-01 09:05:24 +0100

    increase hibernate search version

--- elza/elza-core/src/main/java/cz/tacr/elza/domain/LuceneAnalyzerConfigurer.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/domain/LuceneAnalyzerConfigurer.java
@@ -1,7 +1,7 @@
 package cz.tacr.elza.domain;
 
+import org.apache.lucene.analysis.classic.ClassicTokenizerFactory;
 import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
-import org.apache.lucene.analysis.core.WhitespaceTokenizerFactory;
 import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilterFactory;
 import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurationContext;
 import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurer;
@@ -11,8 +11,9 @@ public class LuceneAnalyzerConfigurer implements LuceneAnalysisConfigurer {
 	@Override
     public void configure(LuceneAnalysisConfigurationContext context) {
         context.analyzer("cz").custom()
-        	// https://stackoverflow.com/questions/58542870/getting-a-maxbyteslengthexceededexception-for-a-textfield
-        	.tokenizer(WhitespaceTokenizerFactory.class)
+    		// https://stackoverflow.com/questions/58542870/getting-a-maxbyteslengthexceededexception-for-a-textfield
+        	// https://solr.apache.org/guide/solr/latest/indexing-guide/tokenizers.html
+        	.tokenizer(ClassicTokenizerFactory.class)
         	.tokenFilter(LowerCaseFilterFactory.class)
         	.tokenFilter(ASCIIFoldingFilterFactory.class);
     }
