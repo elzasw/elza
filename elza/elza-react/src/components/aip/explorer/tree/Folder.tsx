d43ba2223ced7fabf045eb24c9da22b0d9f6e09a
Kašparová Alena <alena.kasparova@marbes.cz>
2024-10-29 08:14:58 +0100

    MCW-15380 - ELZA DA: průzkumník - zobrazení komponent v logické struktuře, úpravy

--- elza/elza-react/src/components/aip/explorer/tree/Folder.tsx
+++ elza/elza-react/src/components/aip/explorer/tree/Folder.tsx
@@ -19,6 +19,7 @@ const Folder = ({folder, openItems, parent}: FolderProps) => {
     const isSelected: boolean = selectedItem.uuid == folder.uuid;
     folder.parent = parent;
     const files = folder?.childFiles?.filter(file => !file.isLogical);
+    const isLast = folder?.childFiles?.every(item => item.isLogical) && folder?.childFolders?.length == 0;
 
     const getExpandIcon = () => {
         return openItems.includes(folder.uuid) ? (
@@ -29,10 +30,10 @@ const Folder = ({folder, openItems, parent}: FolderProps) => {
     }
 
     return (
-        <TreeItem itemType="branch" value={folder.uuid} >
+        <TreeItem itemType={isLast ? "leaf" : "branch"} value={folder.uuid} >
             <TreeItemLayout
                 style={{backgroundColor: isSelected ?  "#e3e3e3ff" : undefined}}
-                expandIcon={getExpandIcon()}
+                expandIcon={isLast ? undefined : getExpandIcon()}
             >
                 {turncate(folder.label)}
             </TreeItemLayout>
