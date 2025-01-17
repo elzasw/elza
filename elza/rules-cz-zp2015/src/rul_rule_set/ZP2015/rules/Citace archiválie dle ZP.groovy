f82dbb2d754b4052e3a5a7d1297c8c2417750c8a
Petr Pytelka <petr.pytelka@lightcomp.cz>
2025-01-17 11:23:44 +0100

    Refaktorizace generatoru citaci

--- elza/rules-cz-zp2015/src/rul_rule_set/ZP2015/rules/Citace archiválie dle ZP.groovy
+++ elza/rules-cz-zp2015/src/rul_rule_set/ZP2015/rules/Citace archiválie dle ZP.groovy
@@ -12,29 +12,31 @@ import cz.tacr.elza.print.item.convertors.UnitDatePrintConvertor
 
 return generate(GENERATOR_CONTEXT)
 
-String generate(final GroovyGenCtx ctx) {
-    String fundName = ctx.getFund().getName()
-    String fundMark = StringUtils.isEmpty(ctx.getFund().getMark()) ? "" : " (" + ctx.getFund().getMark() + ")"
-
-    String idn = StringUtils.isEmpty(ctx.getInstitutionCode()) ? "-" : ctx.getInstitutionCode()
-    String fundNumber = ctx.getFund().getNumber() == null ? "-" : String.valueOf(ctx.getFund().getNumber())
-    GroovyItem groovyUnitId = ctx.getFirstItemByItemType("ZP2015_UNIT_ID")
-    String unitId = groovyUnitId == null ? "-" : groovyUnitId.getValue()
-    String refOzn = idn + "//" + fundNumber + "//" + unitId
-
+String generate(final GroovyGenCtx ctx) {    
     GroovyItem aeName = GroovyUtils.findItemByPartContains(ctx.getGroovyAe(), "PT_NAME", "NM_MAIN", true)
-    final String ZP2015_OTHER_ID = "ZP2015_OTHER_ID"
 
     rb = new ResultBuilder(aeName, ctx)
     // add fund
-    rb.append(fundName + fundMark)
-
+    rb.append(ctx.getFund().getName());
+    if(!StringUtils.isEmpty(ctx.getFund().getMark())) {
+		// add mark
+		rb.append(" (").append(ctx.getFund().getMark()).append(")");
+	}
+    
     // ref ozn
-    rb.append("ref. ozn. CZ" + refOzn)
-      .appendItem("ZP2015_INV_CISLO", "inv. č. ")
+    GroovyItem groovyUnitId = ctx.getFirstItemByItemType("ZP2015_UNIT_ID");
+    rb.append("ref. ozn. CZ")
+      .append(StringUtils.isEmpty(ctx.getInstitutionCode()) ? "-" : ctx.getInstitutionCode())
+      .append("//")
+      .append(ctx.getFund().getNumber() == null ? "-" : String.valueOf(ctx.getFund().getNumber()))
+      .append("//")
+      .append((groovyUnitId == null)  ? "-" : groovyUnitId.getValue());
+        
+    rb.appendItem("ZP2015_INV_CISLO", "inv. č. ")
       .appendItem("ZP2015_SERIAL_NUMBER", "poř. č. ")
 
     // jina ozn
+    final String ZP2015_OTHER_ID = "ZP2015_OTHER_ID";
     rb.appendItemWithSpecLabel(ZP2015_OTHER_ID, "ZP2015_OTHERID_SIG_ORIG")
       .appendItemWithSpecLabel(ZP2015_OTHER_ID, "ZP2015_OTHERID_SIG")
       .appendItemWithSpecLabel(ZP2015_OTHER_ID, "ZP2015_OTHERID_STORAGE_ID")
