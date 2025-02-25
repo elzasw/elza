628dd0a2c83d83b1b5bf7f495ac259ff35b5d052
Sergey Iryupin <biblelamp@gmail.com>
2025-02-25 14:08:28 +0100

    #9341 oprava ApDetailPageWrapper.tsx

--- elza/elza-react/src/components/registry/ApDetailPageWrapper.tsx
+++ elza/elza-react/src/components/registry/ApDetailPageWrapper.tsx
@@ -614,7 +614,7 @@ const mapDispatchToProps = (dispatch: ThunkDispatch<AppState, any, Action<string
         dispatch(DetailActions.fetchIfNeeded(
             AP_VALIDATION,
             apId,
-            (id: number) => Api.accesspoints.accessPointValidateAccessPoint(id.toString(), includeRevision),
+            (id: number) => Api.accesspoints.accessPointValidateAccessPoint(id, includeRevision),
             true
         ));
     },
