764ab8b98f2bbf3085d297a3f64aaa64cad0e376
Petr Pytelka <petr.pytelka@lightcomp.cz>
2025-07-03 10:06:07 +0200

    #9453 - Chyba při vyhledávání v tabulkovém zobrazení - Pořadí uložení a Role entit

--- elza/elza-core/src/main/java/cz/tacr/elza/repository/DataRepositoryImpl.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/repository/DataRepositoryImpl.java
@@ -221,8 +221,12 @@ public class DataRepositoryImpl implements DataRepositoryCustom {
         selections.add(valueExpression);
 
         //oříznutá hodnota převedená na string, kvůli řazení
-        Expression<String> substringValue = builder.substring(valueSelection.as(String.class), 0, 100);
-        selections.add(substringValue);
+        if(dataTypeClass==ArrDataString.class || dataTypeClass==ArrDataText.class) {
+            Expression<String> substringValue = builder.substring(valueSelection.as(String.class), 0, 100);
+            selections.add(substringValue);        	
+        } else {
+        	selections.add(valueSelection);
+        }
 
         //id odkazovaného objektu
         Path<Integer> idPath = typeHelper.getIdSelection(builder);
