b21fe105c8ce5c69db7f4d552b354fb9da322e4f
Petr Pytelka <petr.pytelka@lightcomp.cz>
2025-07-12 00:55:18 +0200

    Automaticke odstraneni predchozi vazby v externim systemu pri zmene teto vazby

--- elza/elza-core/src/main/java/cz/tacr/elza/service/cam/CamService.java
+++ elza/elza-core/src/main/java/cz/tacr/elza/service/cam/CamService.java
@@ -355,7 +355,7 @@ public class CamService {
                               Map<Integer, String> partUuidMap,
                               Map<Integer, String> stateMap, 
                               BatchInfoXml batchInfoXml) {
-    	log.debug("Updating binding, extSyncsQueueItemId: {}", extSyncsQueueItem.getExtSyncsQueueItemId());
+    	log.debug("Updating binding, extSyncsQueueItemId: {}, accessPointId: {}", extSyncsQueueItem.getExtSyncsQueueItemId(), extSyncsQueueItem.getAccessPointId());
     	
         ApState state = accessPointService.getStateInternal(extSyncsQueueItem.getAccessPointId());
         ApAccessPoint accessPoint = state.getAccessPoint();
@@ -384,8 +384,31 @@ public class CamService {
                                                                     accessPoint.getPreferredPart(),
                                                                     state.getApType());
         } else {
-            binding = externalSystemService.createApBinding(Long.toString(batchEntityRecordRev.getEid().getValue()),
-                                                            apExternalSystem, true);
+        	String bindingValue = Long.toString(batchEntityRecordRev.getEid().getValue());
+        	// check if binding exists
+			binding = externalSystemService.findByValueAndExternalSystem(bindingValue, apExternalSystem);
+			if (binding == null) {
+				// binding does not exist
+				binding = externalSystemService.createApBinding(bindingValue, apExternalSystem, true);
+			} else {
+				log.debug("Found existing binding, bindingId: {}, disconnecting existing binding", binding.getBindingId());
+				// invalidate existing binding state
+				// this usually happen in test environment where binding is used in multiple access points
+				// in production it should not happen
+				// we can add some condition to control this behavior
+				var curBindingState = bindingStateRepository.findActiveByBinding(binding);
+				if (curBindingState.isPresent()) {
+					log.info("Deleting previouse binding state, bindingStateId: {}", curBindingState.get().getBindingStateId());
+					curBindingState.get().setDeleteChange(change);					
+					bindingStateRepository.saveAndFlush(curBindingState.get());
+				}
+				var bindingItems = bindingItemRepository.findByBinding(binding);
+				for(var bindingItem : bindingItems) {
+					log.info("Deleting previouse binding item, bindingItemId: {}, value: {}", bindingItem.getBindingId(), bindingItem.getValue());
+					bindingItem.setDeleteChange(change);
+					bindingItemRepository.saveAndFlush(bindingItem);
+				}
+			}
             bindingState = externalSystemService.createBindingState(binding, accessPoint, change, camApState,
                                                                     batchEntityRecordRev.getRev().getValue(),
                                                                     userName.getValue(), null, SyncState.SYNC_OK,
@@ -394,13 +417,14 @@ public class CamService {
         }
 
         // Create bindings
+        var finalBinding = binding;
         itemUuidMap.forEach((itemId, value) -> {
             ApItem item = entityManager.getReference(ApItem.class, itemId);
-            this.externalSystemService.createApBindingItem(binding, change, value, null, item);
+            this.externalSystemService.createApBindingItem(finalBinding, change, value, null, item);
         });
         partUuidMap.forEach((partId, value) -> {
             ApPart part = entityManager.getReference(ApPart.class, partId);
-            this.externalSystemService.createApBindingItem(binding, change, value, part, null);
+            this.externalSystemService.createApBindingItem(finalBinding, change, value, part, null);
         });
 
         setQueueItemState(Collections.singletonList(extSyncsQueueItem), ExtAsyncQueueState.EXPORT_OK, OffsetDateTime.now(), null);
